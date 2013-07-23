88b363400f8fd612e8703f08a94e30b42b7aa11b
Vladimír Vondruš <mosra@centrum.cz>
2013-07-23 13:49:16 +0200

    Fixed visibility macros for plugins used as dependencies.
    The symbols weren't exported on mingw32, causing linker errors.

--- src/FreeTypeFont/FreeTypeFont.h
+++ src/FreeTypeFont/FreeTypeFont.h
@@ -37,7 +37,7 @@ typedef FT_LibraryRec_* FT_Library;
 struct FT_FaceRec_;
 typedef FT_FaceRec_*  FT_Face;
 
-#ifdef FreeTypeFont_EXPORTS
+#if defined(FreeTypeFont_EXPORTS) || defined(FreeTypeFontObjects_EXPORTS)
     #define MAGNUM_TEXT_FREETYPEFONT_EXPORT CORRADE_VISIBILITY_EXPORT
 #else
     #define MAGNUM_TEXT_FREETYPEFONT_EXPORT CORRADE_VISIBILITY_IMPORT
