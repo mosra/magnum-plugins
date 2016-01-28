fae761ed0207c11c100e0fa2c57c9df4652b0955
Vladimír Vondruš <mosra@centrum.cz>
2016-01-28 15:42:57 +0100

    pacakge: added Homebrew formula.

---
+++ package/homebrew/magnum-plugins.rb
@@ -0,0 +1,23 @@
+# kate: indent-width 2;
+
+class MagnumPlugins < Formula
+  desc "Plugins for Magnum graphics engine"
+  homepage "https://github.com/mosra/magnum"
+  head "git://github.com/mosra/magnum-plugins.git"
+
+  depends_on "cmake"
+  depends_on "magnum"
+  depends_on "freetype"
+  depends_on "harfbuzz"
+  depends_on "libpng"
+  depends_on "jpeg"
+
+  def install
+    system "mkdir build"
+    cd "build" do
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_ANYAUDIOIMPORTER=ON", "-DWITH_ANYIMAGECONVERTER=ON", "-DWITH_ANYIMAGEIMPORTER=ON", "-DWITH_ANYSCENEIMPORTER=ON", "-DWITH_DDSIMPORTER=ON", "-DWITH_FREETYPEFONT=ON", "-DWITH_HARFBUZZFONT=ON", "-DWITH_JPEGIMPORTER=ON", "-DWITH_MINIEXRIMAGECONVERTER=ON", "-DWITH_OPENGEXIMPORTER=ON", "-DWITH_PNGIMPORTER=ON", "-DWITH_STANFORDIMPORTER=ON", "-DWITH_STBIMAGEIMPORTER=ON", "-DWITH_STBPNGIMAGECONVERTER=ON", "-DWITH_STBVORBISAUDIOIMPORTER=ON", ".."
+      system "cmake", "--build", "."
+      system "cmake", "--build", ".", "--target", "install"
+    end
+  end
+end
