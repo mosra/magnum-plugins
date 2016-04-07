31691afc2c0604d5edb86df46898c238a82b2eab
Squareys <Squareys@googlemail.com>
2016-04-07 12:29:24 +0200

    modules: Use `endforeach()` to close logical foreach block
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- modules/FindMagnumPlugins.cmake
+++ modules/FindMagnumPlugins.cmake
@@ -298,7 +298,7 @@ foreach(_component ${MagnumPlugins_FIND_COMPONENTS})
             foreach(_dependency ${_MAGNUMPLUGINS_${_COMPONENT}_MAGNUM_DEPENDENCIES})
                 set_property(TARGET MagnumPlugins::${_component} APPEND PROPERTY
                     INTERFACE_LINK_LIBRARIES Magnum:${_dependency})
-            endif()
+            endforeach()
 
             # Add inter-project dependencies
             foreach(_dependency ${_MAGNUMPLUGINS_${_COMPONENT}_DEPENDENCIES})
