b8346519ee029b0ed5c32322c91a9abdbaeace4f
Vladimír Vondruš <mosra@centrum.cz>
2016-08-01 23:28:15 +0200

    package/ci: build for Emscripten and iOS Simulator on Travis.

---
+++ package/ci/travis-desktop.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -ev
+
+# Corrade
+git clone --depth 1 git://github.com/mosra/corrade.git
+cd corrade
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_INTERCONNECT=OFF
+make -j install
+cd ../..
+
+# Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git
+cd magnum
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_AUDIO=ON \
+    -DWITH_DEBUGTOOLS=OFF \
+    -DWITH_PRIMITIVES=OFF \
+    -DWITH_SCENEGRAPH=OFF \
+    -DWITH_SHADERS=OFF \
+    -DWITH_SHAPES=OFF \
+    -DWITH_TEXT=ON \
+    -DWITH_TEXTURETOOLS=ON \
+    -DWITH_WINDOWLESS${PLATFORM_GL_API}APPLICATION=ON
+make -j install
+cd ../..
+
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_PREFIX_PATH="$HOME/deps;$HOME/harfbuzz" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_ANYAUDIOIMPORTER=ON \
+    -DWITH_ANYIMAGECONVERTER=ON \
+    -DWITH_ANYIMAGEIMPORTER=ON \
+    -DWITH_ANYSCENEIMPORTER=ON \
+    -DWITH_COLLADAIMPORTER=OFF \
+    -DWITH_DDSIMPORTER=ON \
+    -DWITH_FREETYPEFONT=ON \
+    -DWITH_HARFBUZZFONT=ON \
+    -DWITH_JPEGIMPORTER=ON \
+    -DWITH_MINIEXRIMAGECONVERTER=ON \
+    -DWITH_OPENGEXIMPORTER=ON \
+    -DWITH_PNGIMAGECONVERTER=ON \
+    -DWITH_PNGIMPORTER=ON \
+    -DWITH_STANFORDIMPORTER=ON \
+    -DWITH_STBIMAGEIMPORTER=ON \
+    -DWITH_STBPNGIMAGECONVERTER=ON \
+    -DWITH_STBTRUETYPEFONT=ON \
+    -DWITH_STBVORBISAUDIOIMPORTER=ON \
+    -DBUILD_TESTS=ON \
+    -DBUILD_GL_TESTS=ON
+make -j${JOBS_LIMIT}
+CORRADE_TEST_COLOR=ON ctest -V -E GLTest
---
+++ package/ci/travis-emscripten.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -ev
+
+git submodule update --init
+
+git clone --depth 1 git://github.com/mosra/corrade.git
+cd corrade
+
+# Build native corrade-rc
+mkdir build && cd build || exit /b
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps-native/lib \
+    -DWITH_INTERCONNECT=OFF \
+    -DWITH_PLUGINMANAGER=OFF \
+    -DWITH_TESTSUITE=OFF
+make -j install
+cd ..
+
+# Crosscompile Corrade
+mkdir build-emscripten && cd build-emscripten
+cmake .. \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_TOOLCHAIN_FILE="../../toolchains/generic/Emscripten.cmake" \
+    -DEMSCRIPTEN_PREFIX=$(echo /usr/local/Cellar/emscripten/*/libexec) \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O1" \
+    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O1" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DWITH_INTERCONNECT=OFF
+make -j install
+cd ../..
+
+# Crosscompile Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git
+cd magnum
+mkdir build-emscripten && cd build-emscripten
+cmake .. \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_TOOLCHAIN_FILE="../../toolchains/generic/Emscripten.cmake" \
+    -DEMSCRIPTEN_PREFIX=$(echo /usr/local/Cellar/emscripten/*/libexec) \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O1" \
+    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O1" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
+    -DWITH_AUDIO=OFF \
+    -DWITH_DEBUGTOOLS=OFF \
+    -DWITH_PRIMITIVES=OFF \
+    -DWITH_SCENEGRAPH=OFF \
+    -DWITH_SHADERS=OFF \
+    -DWITH_SHAPES=OFF \
+    -DWITH_TEXT=ON \
+    -DWITH_TEXTURETOOLS=ON \
+    -DTARGET_GLES2=$TARGET_GLES2
+make -j install
+cd ../..
+
+# Crosscompile
+mkdir build-emscripten && cd build-emscripten
+cmake .. \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_TOOLCHAIN_FILE="../toolchains/generic/Emscripten.cmake" \
+    -DEMSCRIPTEN_PREFIX=$(echo /usr/local/Cellar/emscripten/*/libexec) \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O1" \
+    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O1" \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
+    -DWITH_ANYAUDIOIMPORTER=OFF \
+    -DWITH_ANYIMAGECONVERTER=ON \
+    -DWITH_ANYIMAGEIMPORTER=ON \
+    -DWITH_ANYSCENEIMPORTER=ON \
+    -DWITH_COLLADAIMPORTER=OFF \
+    -DWITH_DDSIMPORTER=ON \
+    -DWITH_FREETYPEFONT=OFF \
+    -DWITH_HARFBUZZFONT=OFF \
+    -DWITH_JPEGIMPORTER=OFF \
+    -DWITH_MINIEXRIMAGECONVERTER=ON \
+    -DWITH_OPENGEXIMPORTER=ON \
+    -DWITH_PNGIMAGECONVERTER=OFF \
+    -DWITH_PNGIMPORTER=OFF \
+    -DWITH_STANFORDIMPORTER=ON \
+    -DWITH_STBIMAGEIMPORTER=ON \
+    -DWITH_STBPNGIMAGECONVERTER=ON \
+    -DWITH_STBTRUETYPEFONT=ON \
+    -DWITH_STBVORBISAUDIOIMPORTER=OFF \
+    -DBUILD_TESTS=ON
+make -j4
+
+# Test
+CORRADE_TEST_COLOR=ON ctest -V
