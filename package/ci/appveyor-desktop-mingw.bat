73c0486f4d092e8e5d8f59059e08eb40d42442a9
Vladimír Vondruš <mosra@centrum.cz>
2016-09-01 23:44:29 +0200

    package/ci: build for MinGW and Windows RT GLES2/GLES3 on AppVeyor.
    Similar workflow to what's done in Corrade and Magnum repos. Finally.
    Also:
    
     * Using LibJPEG Turbo 1.5.0 instead of 1.2.0 because the older one
       doesn't work with MinGW at all.
     * Enabling TgaImporter, ObjImporter and WavAudioImporter in Magnum in
       order to be able to test the Any*Importer plugins.

---
+++ package/ci/appveyor-desktop-mingw.bat
@@ -0,0 +1,88 @@
+rem Workaround for CMake not wanting sh.exe on PATH for MinGW. AARGH.
+set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set PATH=C:\tools\mingw64\bin;%APPVEYOR_BUILD_FOLDER%/openal/bin/Win64;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+
+rem Build LibJPEG
+IF NOT EXIST %APPVEYOR_BUILD_FOLDER%\libjpeg-turbo-1.5.0.tar.gz appveyor DownloadFile http://downloads.sourceforge.net/project/libjpeg-turbo/1.5.0/libjpeg-turbo-1.5.0.tar.gz || exit /b
+7z x libjpeg-turbo-1.5.0.tar.gz || exit /b
+7z x libjpeg-turbo-1.5.0.tar || exit /b
+ren libjpeg-turbo-1.5.0 libjpeg-turbo || exit /b
+cd libjpeg-turbo || exit /b
+mkdir build && cd build || exit /b
+cmake .. -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_JPEG8=ON ^
+    -DWITH_SIMD=OFF ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . --target install -- -j || exit /b
+cd .. && cd .. || exit /b
+
+rem Build Corrade. Could not get Ninja to work, meh.
+git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
+cd corrade || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_INTERCONNECT=OFF ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . -- -j || exit /b
+cmake --build . --target install -- -j || exit /b
+cd .. && cd ..
+
+rem Build Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git || exit /b
+cd magnum || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/openal ^
+    -DWITH_AUDIO=ON ^
+    -DWITH_DEBUGTOOLS=OFF ^
+    -DWITH_PRIMITIVES=OFF ^
+    -DWITH_SCENEGRAPH=OFF ^
+    -DWITH_SHADERS=OFF ^
+    -DWITH_SHAPES=OFF ^
+    -DWITH_TEXT=ON ^
+    -DWITH_TEXTURETOOLS=ON ^
+    -DWITH_WINDOWLESSWGLAPPLICATION=ON ^
+    -DWITH_OBJIMPORTER=ON ^
+    -DWITH_TGAIMPORTER=ON ^
+    -DWITH_WAVAUDIOIMPORTER=ON ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . -- -j || exit /b
+cmake --build . --target install -- -j || exit /b
+cd .. && cd ..
+
+rem Build
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/openal ^
+    -DWITH_ANYAUDIOIMPORTER=ON ^
+    -DWITH_ANYIMAGECONVERTER=ON ^
+    -DWITH_ANYIMAGEIMPORTER=ON ^
+    -DWITH_ANYSCENEIMPORTER=ON ^
+    -DWITH_DDSIMPORTER=ON ^
+    -DWITH_FREETYPEFONT=OFF ^
+    -DWITH_HARFBUZZFONT=OFF ^
+    -DWITH_JPEGIMPORTER=ON ^
+    -DWITH_MINIEXRIMAGECONVERTER=ON ^
+    -DWITH_OPENGEXIMPORTER=ON ^
+    -DWITH_PNGIMAGECONVERTER=OFF ^
+    -DWITH_PNGIMPORTER=OFF ^
+    -DWITH_STANFORDIMPORTER=ON ^
+    -DWITH_STBIMAGEIMPORTER=ON ^
+    -DWITH_STBPNGIMAGECONVERTER=ON ^
+    -DWITH_STBTRUETYPEFONT=ON ^
+    -DWITH_STBVORBISAUDIOIMPORTER=ON ^
+    -DBUILD_TESTS=ON ^
+    -DBUILD_GL_TESTS=ON ^
+    -G "MinGW Makefiles" || exit /b
+cmake --build . -- -j || exit /b
+cmake --build . --target install -- -j || exit /b
+
+rem Test
+ctest -V -E GLTest || exit /b
---
+++ package/ci/appveyor-rt.bat
@@ -0,0 +1,94 @@
+call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" || exit /b
+set PATH=%APPVEYOR_BUILD_FOLDER%\deps-native\bin;%PATH%
+
+rem Build ANGLE
+git clone --depth 1 git://github.com/MSOpenTech/angle.git || exit /b
+cd angle\winrt\10\src || exit /b
+msbuild angle.sln /p:Configuration=Release || exit /b
+cd ..\..\..\.. || exit /b
+
+git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
+cd corrade || exit /b
+
+rem Build native corrade-rc
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps-native ^
+    -DWITH_INTERCONNECT=OFF ^
+    -DWITH_PLUGINMANAGER=OFF ^
+    -DWITH_TESTSUITE=OFF ^
+    -G Ninja || exit /b
+cmake --build . --target install || exit /b
+cd .. || exit /b
+
+rem Crosscompile Corrade
+mkdir build-rt && cd build-rt || exit /b
+cmake .. ^
+    -DCMAKE_SYSTEM_NAME=WindowsStore ^
+    -DCMAKE_SYSTEM_VERSION=10.0 ^
+    -DCORRADE_RC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/deps-native/bin/corrade-rc.exe ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_INTERCONNECT=OFF ^
+    -DBUILD_STATIC=ON ^
+    -G "Visual Studio 14 2015" || exit /b
+cmake --build . --config Release --target install -- /m /v:m || exit /b
+cd .. && cd ..
+
+rem Crosscompile Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git || exit /b
+cd magnum || exit /b
+mkdir build-rt && cd build-rt || exit /b
+cmake .. ^
+    -DCMAKE_SYSTEM_NAME=WindowsStore ^
+    -DCMAKE_SYSTEM_VERSION=10.0 ^
+    -DCORRADE_RC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/deps-native/bin/corrade-rc.exe ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DOPENGLES2_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_Win32/lib/libGLESv2.lib ^
+    -DOPENGLES2_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
+    -DOPENGLES3_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_Win32/lib/libGLESv2.lib ^
+    -DOPENGLES3_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
+    -DWITH_AUDIO=OFF ^
+    -DWITH_DEBUGTOOLS=OFF ^
+    -DWITH_PRIMITIVES=OFF ^
+    -DWITH_SCENEGRAPH=OFF ^
+    -DWITH_SHADERS=OFF ^
+    -DWITH_SHAPES=OFF ^
+    -DWITH_TEXT=ON ^
+    -DWITH_TEXTURETOOLS=ON ^
+    -DTARGET_GLES2=%TARGET_GLES2% ^
+    -DBUILD_STATIC=ON ^
+    -G "Visual Studio 14 2015" || exit /b
+cmake --build . --config Release --target install -- /m /v:m || exit /b
+cd .. && cd ..
+
+rem Crosscompile
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DOPENGLES2_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_Win32/lib/libGLESv2.lib ^
+    -DOPENGLES2_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
+    -DOPENGLES3_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_Win32/lib/libGLESv2.lib ^
+    -DOPENGLES3_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
+    -DWITH_ANYAUDIOIMPORTER=OFF ^
+    -DWITH_ANYIMAGECONVERTER=ON ^
+    -DWITH_ANYIMAGEIMPORTER=ON ^
+    -DWITH_ANYSCENEIMPORTER=ON ^
+    -DWITH_DDSIMPORTER=ON ^
+    -DWITH_FREETYPEFONT=OFF ^
+    -DWITH_HARFBUZZFONT=OFF ^
+    -DWITH_JPEGIMPORTER=OFF ^
+    -DWITH_MINIEXRIMAGECONVERTER=ON ^
+    -DWITH_OPENGEXIMPORTER=ON ^
+    -DWITH_PNGIMAGECONVERTER=OFF ^
+    -DWITH_PNGIMPORTER=OFF ^
+    -DWITH_STANFORDIMPORTER=ON ^
+    -DWITH_STBIMAGEIMPORTER=ON ^
+    -DWITH_STBPNGIMAGECONVERTER=ON ^
+    -DWITH_STBTRUETYPEFONT=ON ^
+    -DWITH_STBVORBISAUDIOIMPORTER=OFF ^
+    -DBUILD_STATIC=ON ^
+    -G "Visual Studio 14 2015" || exit /b
+cmake --build . --config Release -- /m /v:m || exit /b
