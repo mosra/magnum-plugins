5b253e5e08576abd1866725f96e51ceb5af87e16
Vladimír Vondruš <mosra@centrum.cz>
2016-10-16 02:18:15 +0200

    package/ci: build for Android GLES2/GLES3 on Travis.

---
+++ package/ci/travis-android-arm.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -ev
+
+git submodule update --init
+
+# Corrade
+git clone --depth 1 git://github.com/mosra/corrade.git
+cd corrade
+
+# Build native corrade-rc
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps-native/lib \
+    -DWITH_INTERCONNECT=OFF \
+    -DWITH_PLUGINMANAGER=OFF \
+    -DWITH_TESTSUITE=OFF
+make -j install
+cd ..
+
+# Crosscompile Corrade
+mkdir build-android-arm && cd build-android-arm
+ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DWITH_INTERCONNECT=OFF
+make -j install
+cd ../..
+
+# Crosscompile Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git
+cd magnum
+mkdir build-android-arm && cd build-android-arm
+ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
+    -DWITH_AUDIO=OFF \
+    -DWITH_DEBUGTOOLS=OFF \
+    -DWITH_MESHTOOLS=OFF \
+    -DWITH_PRIMITIVES=OFF \
+    -DWITH_SCENEGRAPH=OFF \
+    -DWITH_SHADERS=OFF \
+    -DWITH_SHAPES=OFF \
+    -DWITH_TEXT=ON \
+    -DWITH_TEXTURETOOLS=ON \
+    -DTARGET_GLES2=$TARGET_GLES2
+make -j install
+cd ../..
+
+# Crosscompile
+mkdir build-android-arm && cd build-android-arm
+ANDROID_NDK=$TRAVIS_BUILD_DIR/android-ndk-r10e cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/Android-ARM.cmake \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_PREFIX_PATH=$HOME/deps \
+    -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_ANYAUDIOIMPORTER=OFF \
+    -DWITH_ANYIMAGECONVERTER=ON \
+    -DWITH_ANYIMAGEIMPORTER=ON \
+    -DWITH_ANYSCENEIMPORTER=ON \
+    -DWITH_COLLADAIMPORTER=OFF \
+    -DWITH_DDSIMPORTER=ON \
+    -DWITH_DRFLACAUDIOIMPORTER=OFF \
+    -DWITH_DRWAVAUDIOIMPORTER=OFF \
+    -DWITH_FREETYPEFONT=OFF \
+    -DWITH_HARFBUZZFONT=OFF \
+    -DWITH_JPEGIMPORTER=OFF \
+    -DWITH_MINIEXRIMAGECONVERTER=ON \
+    -DWITH_OPENGEXIMPORTER=ON \
+    -DWITH_PNGIMAGECONVERTER=OFF \
+    -DWITH_PNGIMPORTER=OFF \
+    -DWITH_STANFORDIMPORTER=ON \
+    -DWITH_STBIMAGEIMPORTER=ON \
+    -DWITH_STBPNGIMAGECONVERTER=ON \
+    -DWITH_STBTRUETYPEFONT=ON \
+    -DWITH_STBVORBISAUDIOIMPORTER=OFF \
+    -DBUILD_TESTS=ON
+make -j${JOBS_LIMIT}
+
+# Start simulator and run tests
+echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
+emulator -avd test -no-audio -no-window &
+android-wait-for-emulator
+CORRADE_TEST_COLOR=ON ctest -V
