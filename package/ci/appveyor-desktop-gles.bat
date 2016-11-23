686887d36503d43e918b7affca415def3eae25cf
Vladimír Vondruš <mosra@centrum.cz>
2016-11-23 16:45:37 +0100

    package/ci: build for desktop GLES2/GLES3 on Windows.

---
+++ package/ci/appveyor-desktop-gles.bat
@@ -0,0 +1,93 @@
+call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
+set PATH=%APPVEYOR_BUILD_FOLDER%/openal/bin/Win64;%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
+
+rem Build LibJPEG
+IF NOT EXIST %APPVEYOR_BUILD_FOLDER%\libjpeg-turbo-1.5.0.tar.gz appveyor DownloadFile http://downloads.sourceforge.net/project/libjpeg-turbo/1.5.0/libjpeg-turbo-1.5.0.tar.gz || exit /b
+7z x libjpeg-turbo-1.5.0.tar.gz || exit /b
+7z x libjpeg-turbo-1.5.0.tar || exit /b
+ren libjpeg-turbo-1.5.0 libjpeg-turbo || exit /b
+cd libjpeg-turbo || exit /b
+mkdir build && cd build || exit /b
+cmake .. -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_JPEG8=ON ^
+    -DWITH_SIMD=OFF ^
+    -G Ninja || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd .. || exit /b
+
+rem Build Corrade
+git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
+cd corrade || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DWITH_INTERCONNECT=OFF ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
+
+rem Build Magnum
+git clone --depth 1 git://github.com/mosra/magnum.git || exit /b
+cd magnum || exit /b
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/openal ^
+    -DTARGET_GLES=ON ^
+    -DTARGET_GLES2=%TARGET_GLES2% ^
+    -DTARGET_DESKTOP_GLES=ON ^
+    -DWITH_AUDIO=ON ^
+    -DWITH_DEBUGTOOLS=OFF ^
+    -DWITH_MESHTOOLS=OFF ^
+    -DWITH_PRIMITIVES=OFF ^
+    -DWITH_SCENEGRAPH=OFF ^
+    -DWITH_SHADERS=OFF ^
+    -DWITH_SHAPES=OFF ^
+    -DWITH_TEXT=ON ^
+    -DWITH_TEXTURETOOLS=ON ^
+    -DWITH_WINDOWLESSWGLAPPLICATION=ON ^
+    -DWITH_OBJIMPORTER=ON ^
+    -DWITH_TGAIMPORTER=ON ^
+    -DWITH_WAVAUDIOIMPORTER=ON ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+cd .. && cd ..
+
+rem Build
+mkdir build && cd build || exit /b
+cmake .. ^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
+    -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/openal ^
+    -DWITH_ANYAUDIOIMPORTER=ON ^
+    -DWITH_ANYIMAGECONVERTER=ON ^
+    -DWITH_ANYIMAGEIMPORTER=ON ^
+    -DWITH_ANYSCENEIMPORTER=ON ^
+    -DWITH_DDSIMPORTER=ON ^
+    -DWITH_DRFLACAUDIOIMPORTER=ON ^
+    -DWITH_DRWAVAUDIOIMPORTER=ON ^
+    -DWITH_FREETYPEFONT=OFF ^
+    -DWITH_HARFBUZZFONT=OFF ^
+    -DWITH_JPEGIMPORTER=ON ^
+    -DWITH_MINIEXRIMAGECONVERTER=ON ^
+    -DWITH_OPENGEXIMPORTER=ON ^
+    -DWITH_PNGIMAGECONVERTER=OFF ^
+    -DWITH_PNGIMPORTER=OFF ^
+    -DWITH_STANFORDIMPORTER=ON ^
+    -DWITH_STBIMAGEIMPORTER=ON ^
+    -DWITH_STBPNGIMAGECONVERTER=ON ^
+    -DWITH_STBTRUETYPEFONT=ON ^
+    -DWITH_STBVORBISAUDIOIMPORTER=ON ^
+    -DBUILD_TESTS=ON ^
+    -DBUILD_GL_TESTS=ON ^
+    -G Ninja || exit /b
+cmake --build . || exit /b
+cmake --build . --target install || exit /b
+
+rem Test
+ctest -V -E GLTest || exit /b
