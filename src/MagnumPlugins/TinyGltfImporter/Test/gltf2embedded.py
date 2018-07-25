9de97a31df4ae0b852d88275336991614e59da22
Vladimír Vondruš <mosra@centrum.cz>
2018-07-25 17:22:25 +0200

    TinyGltfImporter: tool for creating embedded URIs from buffers/images.

---
+++ src/MagnumPlugins/TinyGltfImporter/Test/gltf2embedded.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+#
+#   This file is part of Magnum.
+#
+#   Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018
+#             Vladimír Vondruš <mosra@centrum.cz>
+#   Copyright © 2018 Jonathan Hale <squareys@googlemail.com>
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the "Software"),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included
+#   in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+
+import base64
+import json
+import os
+import sys
+import struct
+
+fileIn = sys.argv[1]
+fileOut = os.path.splitext(fileIn)[0] + '-embedded.gltf'
+
+print("Converting to", fileOut)
+
+with open(fileIn) as f:
+    data = json.load(f)
+    if 'buffers' in data:
+        for buffer in data['buffers']:
+            uri = buffer['uri']
+            if uri[:5] != 'data:':
+                with open(uri, "rb") as bf:
+                    buffer['uri'] = 'data:application/octet-stream;base64,' + base64.b64encode(bf.read()).decode('utf-8')
+
+    if 'images' in data:
+        for image in data['images']:
+            if 'uri' not in image: continue
+            uri = image['uri']
+            if uri[:5] != 'data:':
+                assert(uri[-4:] == '.png')
+                with open(uri, "rb") as bf:
+                    image['uri'] = 'data:image/png;base64,' + base64.b64encode(bf.read()).decode('utf-8')
+
+with open(fileOut, 'wb') as output:
+    output.write(json.dumps(data, separators=(',', ':')).encode())
