8fc2f085a6b3b97ea7f0e4101d6bec3b3490e2c6
Vladimír Vondruš <mosra@centrum.cz>
2018-10-23 18:18:16 +0200

    FreeTypeFont: check that reinitialization works properly.
    It doesn't, booms on an assert.

---
+++ src/MagnumPlugins/FreeTypeFont/Test/FreeTypeFontReinitializeTest.cpp
@@ -0,0 +1,53 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018
+              Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Corrade/TestSuite/Tester.h>
+
+#include "MagnumPlugins/FreeTypeFont/FreeTypeFont.h"
+
+namespace Magnum { namespace Text { namespace Test {
+
+struct FreeTypeFontReinitializeTest: TestSuite::Tester {
+    explicit FreeTypeFontReinitializeTest();
+
+    void test();
+};
+
+FreeTypeFontReinitializeTest::FreeTypeFontReinitializeTest() {
+    addTests({&FreeTypeFontReinitializeTest::test});
+}
+
+void FreeTypeFontReinitializeTest::test() {
+    FreeTypeFont::initialize();
+    FreeTypeFont::finalize();
+    FreeTypeFont::initialize();
+    FreeTypeFont::finalize();
+
+    CORRADE_VERIFY(true); /* Yay, nothing crashed! */
+}
+
+}}}
+
+CORRADE_TEST_MAIN(Magnum::Text::Test::FreeTypeFontReinitializeTest)
