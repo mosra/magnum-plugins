5cbd3b4ea88e3781153eb37dd2c4b108107c2e2e
Vladimír Vondruš <mosra@centrum.cz>
2019-03-09 19:08:43 +0100

    package/ci: noo, we don't build ANGLE anymore.
    *How* did I miss this here?

--- package/ci/appveyor-rt.bat
+++ package/ci/appveyor-rt.bat
@@ -4,12 +4,6 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" set GENERATOR=Visual 
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" set GENERATOR=Visual Studio 15 2017
 set PATH=%APPVEYOR_BUILD_FOLDER%\deps-native\bin;%PATH%
 
-rem Build ANGLE
-git clone --depth 1 git://github.com/MSOpenTech/angle.git || exit /b
-cd angle\winrt\10\src || exit /b
-msbuild angle.sln /p:Configuration=Release || exit /b
-cd ..\..\..\.. || exit /b
-
 git clone --depth 1 git://github.com/mosra/corrade.git || exit /b
 cd corrade || exit /b
 
@@ -49,10 +43,6 @@ cmake .. ^
     -DCORRADE_RC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/deps-native/bin/corrade-rc.exe ^
     -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/deps ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
-    -DOPENGLES2_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_x64/lib/libGLESv2.lib ^
-    -DOPENGLES2_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
-    -DOPENGLES3_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_x64/lib/libGLESv2.lib ^
-    -DOPENGLES3_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
     -DWITH_AUDIO=OFF ^
     -DWITH_DEBUGTOOLS=OFF ^
     -DWITH_GL=OFF ^
@@ -69,16 +59,12 @@ cmake .. ^
 cmake --build . --config Release --target install -- /m /v:m || exit /b
 cd .. && cd ..
 
-rem Crosscompile. BUILD_GL_TESTS is enabled just to be sure, it should not be
-rem needed by any plugin.
+rem Crosscompile
 mkdir build-rt && cd build-rt || exit /b
 cmake .. ^
-    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_SYSTEM_NAME=WindowsStore ^
+    -DCMAKE_SYSTEM_VERSION=10.0 ^
     -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/deps ^
-    -DOPENGLES2_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_x64/lib/libGLESv2.lib ^
-    -DOPENGLES2_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
-    -DOPENGLES3_LIBRARY=%APPVEYOR_BUILD_FOLDER%/angle/winrt/10/src/Release_x64/lib/libGLESv2.lib ^
-    -DOPENGLES3_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/angle/include ^
     -DWITH_ASSIMPIMPORTER=OFF ^
     -DWITH_DDSIMPORTER=ON ^
     -DWITH_DEVILIMAGEIMPORTER=OFF ^
