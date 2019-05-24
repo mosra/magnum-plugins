389de739e2410506260d015e416ceec522008f37
williamjcm <williamjcm@users.noreply.github.com>
2019-05-24 11:37:50 +0200

    New DrMp3AudioImporter plugin using dr_mp3.

---
+++ src/MagnumPlugins/DrMp3AudioImporter/DrMp3Importer.cpp
@@ -0,0 +1,123 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
+              Vladimír Vondruš <mosra@centrum.cz>
+    Copyright © 2019 Guillaume Jacquemin <williamjcm@users.noreply.github.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "DrMp3Importer.h"
+
+#include <Corrade/Containers/ScopeGuard.h>
+#include <Corrade/Utility/Assert.h>
+#include <Corrade/Utility/Debug.h>
+#include <Corrade/Utility/Endianness.h>
+
+#include <Magnum/Math/Packing.h>
+
+#define DR_MP3_IMPLEMENTATION
+#include "dr_mp3.h"
+
+namespace Magnum { namespace Audio {
+
+namespace {
+
+#define _v(value) BufferFormat::value
+/* number of channels = 1-8, number of bytes = 1-4 */
+const BufferFormat mp3FormatTable[8][4] = {
+    {BufferFormat{}, _v(Mono16),   BufferFormat{}, BufferFormat{}}, /* Mono */
+    {BufferFormat{}, _v(Stereo16), BufferFormat{}, BufferFormat{}}, /* Stereo */
+    {BufferFormat{}, BufferFormat{}, BufferFormat{}, BufferFormat{}}, /* Not a thing */
+    {BufferFormat{}, _v(Quad16), BufferFormat{}, BufferFormat{}},    /* Quad */
+    {BufferFormat{}, BufferFormat{}, BufferFormat{}, BufferFormat{}}, /* Also not a thing */
+    {BufferFormat{}, _v(Surround51Channel16), BufferFormat{}, BufferFormat{}}, /* 5.1 */
+    {BufferFormat{}, _v(Surround61Channel16), BufferFormat{}, BufferFormat{}}, /* 6.1 */
+    {BufferFormat{}, _v(Surround71Channel16), BufferFormat{}, BufferFormat{}}  /* 7.1 */
+};
+#undef _v
+
+}
+
+DrMp3Importer::DrMp3Importer() = default;
+
+DrMp3Importer::DrMp3Importer(PluginManager::AbstractManager& manager, const std::string& plugin): AbstractImporter{manager, plugin} {}
+
+auto DrMp3Importer::doFeatures() const -> Features { return Feature::OpenData; }
+
+bool DrMp3Importer::doIsOpened() const { return _data; }
+
+void DrMp3Importer::doOpenData(Containers::ArrayView<const char> data) {
+    drmp3_config config;
+    config.outputChannels = config.outputSampleRate = 0;
+    drmp3_uint64 frameCount;
+
+    drmp3_int16* decodedData = drmp3_open_memory_and_read_s16(data.data(), data.size(), &config, &frameCount);
+
+    if(!decodedData) {
+        Error() << "Audio::DrMp3Importer::openData(): failed to open and decode MP3 data";
+        return;
+    }
+
+    Containers::ScopeGuard drmp3Free{decodedData, drmp3_free};
+
+    const std::uint32_t numChannels = config.outputChannels;
+
+    if(numChannels == 0 || numChannels == 3 || numChannels == 5 || numChannels > 8) {
+        Error() << "Audio::DrMp3Importer::openData(): unsupported channel count"
+                << numChannels;
+        return;
+    }
+
+    /* Can't load something with no samples */
+    if(frameCount == 0) {
+        Error() << "Audio::DrMp3Importer::openData(): no samples";
+        return;
+    }
+
+    _frequency = config.outputSampleRate;
+    _format = mp3FormatTable[numChannels - 1][1];
+    CORRADE_INTERNAL_ASSERT(_format != BufferFormat{});
+
+    const char* const dataBegin = reinterpret_cast<const char*>(decodedData);
+    const char* const dataEnd = reinterpret_cast<const char*>(decodedData + frameCount);
+
+    _data = Containers::Array<char>(frameCount*sizeof(Short));
+    std::copy(dataBegin, dataEnd, _data.begin());
+
+    return;
+}
+
+void DrMp3Importer::doClose() { _data = nullptr; }
+
+BufferFormat DrMp3Importer::doFormat() const { return _format; }
+
+UnsignedInt DrMp3Importer::doFrequency() const { return _frequency; }
+
+Containers::Array<char> DrMp3Importer::doData() {
+    Containers::Array<char> copy(_data.size());
+    std::copy(_data.begin(), _data.end(), copy.begin());
+    return copy;
+}
+
+}}
+
+CORRADE_PLUGIN_REGISTER(DrMp3AudioImporter, Magnum::Audio::DrMp3Importer,
+    "cz.mosra.magnum.Audio.AbstractImporter/0.1")
---
+++ src/MagnumPlugins/DrMp3AudioImporter/DrMp3Importer.h
@@ -0,0 +1,117 @@
+#ifndef Magnum_Audio_DrMp3Importer_h
+#define Magnum_Audio_DrMp3Importer_h
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
+              Vladimír Vondruš <mosra@centrum.cz>
+    Copyright © 2019 Guillaume Jacquemin <williamjcm@users.noreply.github.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+/** @file
+ * @brief Class @ref Magnum::Audio::DrMp3Importer
+ */
+
+#include <Corrade/Containers/Array.h>
+#include <Magnum/Audio/AbstractImporter.h>
+
+#include "MagnumPlugins/DrMp3AudioImporter/configure.h"
+
+#ifndef DOXYGEN_GENERATING_OUTPUT
+#ifndef MAGNUM_DRMP3AUDIOIMPORTER_BUILD_STATIC
+    #ifdef DrMp3AudioImporter_EXPORTS
+        #define MAGNUM_DRMP3AUDIOIMPORTER_EXPORT CORRADE_VISIBILITY_EXPORT
+    #else
+        #define MAGNUM_DRMP3AUDIOIMPORTER_EXPORT CORRADE_VISIBILITY_IMPORT
+    #endif
+#else
+    #define MAGNUM_DRMP3AUDIOIMPORTER_EXPORT CORRADE_VISIBILITY_STATIC
+#endif
+#define MAGNUM_DRMP3AUDIOIMPORTER_LOCAL CORRADE_VISIBILITY_LOCAL
+#else
+#define MAGNUM_DRMP3AUDIOIMPORTER_EXPORT
+#define MAGNUM_DRMP3AUDIOIMPORTER_LOCAL
+#endif
+
+namespace Magnum { namespace Audio {
+
+/**
+@brief MP3 audio importer plugin using dr_mp3
+
+@m_keywords{DrMp3AudioImporter Mp3AudioImporter}
+
+Supports mono, stereo and surround sound files of the following formats using
+the [dr_mp3](https://github.com/mackron/dr_libs) library:
+
+-   16 bit-per-channel, imported as @ref BufferFormat::Mono16,
+    @ref BufferFormat::Stereo16, @ref BufferFormat::Quad16,
+    @ref BufferFormat::Surround51Channel16, @ref BufferFormat::Surround61Channel16
+    or @ref BufferFormat::Surround71Channel16
+
+This plugin depends on the @ref Audio library and is built if
+`WITH_DRMP3AUDIOIMPORTER` is enabled when building Magnum. To use as a dynamic
+plugin, you need to load the @cpp "DrMp3AudioImporter" @ce plugin from
+`MAGNUM_PLUGINS_AUDIOIMPORTER_DIR`. To use as a static plugin or as a
+dependency of another plugin with CMake, you need to request the
+`DrMp3AudioImporter` component of the `MagnumPlugins` package and link to the
+`MagnumPlugins::DrMp3AudioImporter` target. See @ref building-plugins,
+@ref cmake-plugins and @ref plugins for more information.
+
+This plugins provides `Mp3AudioImporter`.
+
+@m_class{m-block m-primary}
+
+@thirdparty This plugin makes use of the
+    [dr_mp3](https://github.com/mackron/dr_libs) library by David Reid,
+    released into the @m_class{m-label m-primary} **public domain**
+    ([license text](https://github.com/mackron/dr_libs/blob/9177e53ba6427e16bd56e436b1dab816e6bc1b77/dr_mp3.h#L3886-L3909),
+    [choosealicense.com](https://choosealicense.com/licenses/unlicense/)),
+    or alternatively under @m_class{m-label m-success} **MIT**
+    ([license text](https://github.com/mackron/dr_libs/blob/9177e53ba6427e16bd56e436b1dab816e6bc1b77/dr_mp3.h#L3911-L3929),
+    [choosealicense.com](https://choosealicense.com/licenses/mit/)).
+
+*/
+class MAGNUM_DRMP3AUDIOIMPORTER_EXPORT DrMp3Importer: public AbstractImporter {
+    public:
+        /** @brief Default constructor */
+        explicit DrMp3Importer();
+
+        /** @brief Plugin manager constructor */
+        explicit DrMp3Importer(PluginManager::AbstractManager& manager, const std::string& plugin);
+
+    private:
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL Features doFeatures() const override;
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL bool doIsOpened() const override;
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL void doOpenData(Containers::ArrayView<const char> data) override;
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL void doClose() override;
+
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL BufferFormat doFormat() const override;
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL UnsignedInt doFrequency() const override;
+        MAGNUM_DRMP3AUDIOIMPORTER_LOCAL Containers::Array<char> doData() override;
+
+        Containers::Array<char> _data;
+        BufferFormat _format;
+        UnsignedInt _frequency;
+};
+
+}}
+
+#endif
