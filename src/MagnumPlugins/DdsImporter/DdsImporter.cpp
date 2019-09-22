401ea7778c040dd2152d3a7ae3a23fe710414350
Vladimír Vondruš <mosra@centrum.cz>
2019-09-22 21:43:06 +0200

    DdsImporter: get rid of the giant error-prone format switch.
    Instead go with a mapping table that does the same, but in a more
    compact and easier to optimize way, along with an ability to print
    string identifiers for unsupported formats.
    
    No functional change, supporting new formats will be done subsequently.

--- src/MagnumPlugins/DdsImporter/DdsImporter.cpp
+++ src/MagnumPlugins/DdsImporter/DdsImporter.cpp
@@ -157,216 +157,30 @@ enum class DdsAlphaMode: UnsignedInt {
     Custom = 4,
 };
 
-enum class DxgiFormat: UnsignedInt {
-    Unknown = 0,
-    R32G32B32A32Typeless = 1,
-    R32G32B32A32Float = 2,
-    R32G32B32A32UInt = 3,
-    R32G32B32A32SInt = 4,
-    R32G32B32Typeless = 5,
-    R32G32B32Float = 6,
-    R32G32B32UInt = 7,
-    R32G32B32SInt = 8,
-    R16G16B16A16Typeless = 9,
-    R16G16B16A16Float = 10,
-    R16G16B16A16UNorm = 11,
-    R16G16B16A16UInt = 12,
-    R16G16B16A16SNorm = 13,
-    R16G16B16A16SInt = 14,
-    R32G32Typeless = 15,
-    R32G32Float = 16,
-    R32G32UInt = 17,
-    R32G32SInt = 18,
-    R32G8X24Typeless = 19,
-    D32FloatS8X24UInt = 20,
-    R32FloatX8X24Typeless = 21,
-    X32TypelessG8X24UInt = 22,
-    R10G10B10A2Typeless = 23,
-    R10G10B10A2UNorm = 24,
-    R10G10B10A2UInt = 25,
-    R11G11B10Float = 26,
-    R8G8B8A8Typeless = 27,
-    R8G8B8A8UNorm = 28,
-    R8G8B8A8UNormSRGB = 29,
-    R8G8B8A8UInt = 30,
-    R8G8B8A8SNorm = 31,
-    R8G8B8A8SInt = 32,
-    R16G16Typeless = 33,
-    R16G16Float = 34,
-    R16G16UNorm = 35,
-    R16G16UInt = 36,
-    R16G16SNorm = 37,
-    R16G16SInt = 38,
-    R32Typeless = 39,
-    D32Float = 40,
-    R32Float = 41,
-    R32UInt = 42,
-    R32SInt = 43,
-    R24G8Typeless = 44,
-    D24UNormS8UInt = 45,
-    R24UNormX8Typeless = 46,
-    X24TypelessG8UInt = 47,
-    R8G8Typeless = 48,
-    R8G8UNorm = 49,
-    R8G8UInt = 50,
-    R8G8SNorm = 51,
-    R8G8SInt = 52,
-    R16Typeless = 53,
-    R16Float = 54,
-    D16UNorm = 55,
-    R16UNorm = 56,
-    R16UInt = 57,
-    R16SNorm = 58,
-    R16SInt = 59,
-    R8Typeless = 60,
-    R8UNorm = 61,
-    R8UInt = 62,
-    R8SNorm = 63,
-    R8SInt = 64,
-    A8UNorm = 65,
-    R1UNorm = 66,
-    R9G9B9E5SharedExp = 67,
-    R8G8B8G8UNorm = 68,
-    G8R8G8B8UNorm = 69,
-    BC1Typeless = 70,
-    BC1UNorm = 71,
-    BC1UNormSRGB = 72,
-    BC2Typeless = 73,
-    BC2UNorm = 74,
-    BC2UNormSRGB = 75,
-    BC3Typeless = 76,
-    BC3UNorm = 77,
-    BC3UNormSRGB = 78,
-    BC4Typeless = 79,
-    BC4UNorm = 80,
-    BC4SNorm = 81,
-    BC5Typeless = 82,
-    BC5UNorm = 83,
-    BC5SNorm = 84,
-    B5G6R5UNorm = 85,
-    B5G5R5A1UNorm = 86,
-    B8G8R8A8UNorm = 87,
-    B8G8R8X8UNorm = 88,
-    R10G10B10XRBIASA2UNorm = 89,
-    B8G8R8A8Typeless = 90,
-    B8G8R8A8UNormSRGB = 91,
-    B8G8R8X8Typeless = 92,
-    B8G8R8X8UNormSRGB = 93,
-    BC6HTypeless = 94,
-    BC6HUF16 = 95,
-    BC6HSF16 = 96,
-    BC7Typeless = 97,
-    BC7UNorm = 98,
-    BC7UNormSRGB = 99,
-    AYUV = 100,
-    Y410 = 101,
-    Y416 = 102,
-    NV12 = 103,
-    P010 = 104,
-    P016 = 105,
-    Opaque420 = 106,
-    YUY2 = 107,
-    Y210 = 108,
-    Y216 = 109,
-    NV11 = 110,
-    AI44 = 111,
-    IA44 = 112,
-    P8 = 113,
-    A8P8 = 114,
-    B4G4R4A4UNorm = 115,
-    P208 = 130,
-    V208 = 131,
-    V408 = 132,
-};
-
 struct DdsHeaderDxt10 {
-    DxgiFormat dxgiFormat;
+    UnsignedInt dxgiFormat;
     DdsDimension resourceDimension;
     DdsMiscFlag miscFlag;
     UnsignedInt arraySize;
     DdsAlphaMode miscFlags2;
 };
 
-PixelFormat dxgiToGl(DxgiFormat format) {
-    switch(format) {
-        /* R8 and A8 formats */
-        case DxgiFormat::R8Typeless:
-        case DxgiFormat::R8UInt:            return PixelFormat::R8UI;
-        case DxgiFormat::R8SInt:            return PixelFormat::R8I;
-        case DxgiFormat::A8UNorm:           /** @todo expose this? */
-        case DxgiFormat::R8UNorm:           return PixelFormat::R8Unorm;
-        case DxgiFormat::R8SNorm:           return PixelFormat::R8Snorm;
-
-        /* RG8 formats */
-        case DxgiFormat::R8G8Typeless:
-        case DxgiFormat::R8G8UInt:          return PixelFormat::RG8UI;
-        case DxgiFormat::R8G8SInt:          return PixelFormat::RG8I;
-        case DxgiFormat::R8G8UNorm:         return PixelFormat::RG8Unorm;
-        case DxgiFormat::R8G8SNorm:         return PixelFormat::RG8Snorm;
-
-        /* RGBA8 formats */
-        case DxgiFormat::R8G8B8A8Typeless:
-        case DxgiFormat::R8G8B8A8UInt:      return PixelFormat::RGBA8UI;
-        case DxgiFormat::R8G8B8A8SInt:      return PixelFormat::RGBA8I;
-        case DxgiFormat::R8G8B8A8UNormSRGB: /** @todo Propagate sRGB property */
-        case DxgiFormat::R8G8B8A8UNorm:     return PixelFormat::RGBA8Unorm;
-        case DxgiFormat::R8G8B8A8SNorm:     return PixelFormat::RGBA8Snorm;
-
-        /* R16 formats */
-        case DxgiFormat::R16Typeless:
-        case DxgiFormat::R16UInt:           return PixelFormat::R16UI;
-        case DxgiFormat::R16SInt:           return PixelFormat::R16I;
-        case DxgiFormat::R16Float:          return PixelFormat::R16F;
-        case DxgiFormat::R16UNorm:          return PixelFormat::R16Unorm;
-        case DxgiFormat::R16SNorm:          return PixelFormat::R16Snorm;
-
-        /* RG16 formats */
-        case DxgiFormat::R16G16Typeless:
-        case DxgiFormat::R16G16UInt:        return PixelFormat::RG16UI;
-        case DxgiFormat::R16G16SInt:        return PixelFormat::RG16I;
-        case DxgiFormat::R16G16Float:       return PixelFormat::RG16F;
-        case DxgiFormat::R16G16UNorm:       return PixelFormat::RG16Unorm;
-        case DxgiFormat::R16G16SNorm:       return PixelFormat::RG16Snorm;
-
-        /* RGBA16 formats */
-        case DxgiFormat::R16G16B16A16Typeless:
-        case DxgiFormat::R16G16B16A16UInt:  return PixelFormat::RGBA16UI;
-        case DxgiFormat::R16G16B16A16SInt:  return PixelFormat::RGBA16I;
-        case DxgiFormat::R16G16B16A16Float: return PixelFormat::RGBA16F;
-        case DxgiFormat::R16G16B16A16UNorm: return PixelFormat::RGBA16Unorm;
-        case DxgiFormat::R16G16B16A16SNorm: return PixelFormat::RGBA16Snorm;
-
-        /* R32 formats */
-        case DxgiFormat::R32Typeless:
-        case DxgiFormat::R32UInt:           return PixelFormat::R32UI;
-        case DxgiFormat::R32SInt:           return PixelFormat::R32I;
-        case DxgiFormat::R32Float:          return PixelFormat::R32F;
-
-        /* RG32 formats */
-        case DxgiFormat::R32G32Typeless:
-        case DxgiFormat::R32G32UInt:        return PixelFormat::RG32UI;
-        case DxgiFormat::R32G32SInt:        return PixelFormat::RG32I;
-        case DxgiFormat::R32G32Float:       return PixelFormat::RG32F;
-
-        /* RGB32 formats */
-        case DxgiFormat::R32G32B32Typeless:
-        case DxgiFormat::R32G32B32UInt:     return PixelFormat::RGB32UI;
-        case DxgiFormat::R32G32B32SInt:     return PixelFormat::RGB32I;
-        case DxgiFormat::R32G32B32Float:    return PixelFormat::RGB32F;
-
-        /* RGBA32 formats */
-        case DxgiFormat::R32G32B32A32Typeless:
-        case DxgiFormat::R32G32B32A32UInt:  return PixelFormat::RGBA32UI;
-        case DxgiFormat::R32G32B32A32SInt:  return PixelFormat::RGBA32I;
-        case DxgiFormat::R32G32B32A32Float: return PixelFormat::RGBA32F;
-
-        /* Unsupported format */
-        case DxgiFormat::D16UNorm: /** @todo expose these in PixelFormat */
-        case DxgiFormat::D32Float:
-        default:
-            return PixelFormat(-1);
-    };
-}
+constexpr struct {
+    /* It still could be packed better (e.g. an union where it's either a name
+       or a format, but let's say this is good enough for now. We're explicitly
+       not storing names of formats we won't ever print. */
+    const char* name;
+    /* Currently there's only about 110 values for compressed formats, so 8
+       bits for each should be enough. Verified in
+       DdsImporterTest::enumValueMatching(). */
+    UnsignedByte format;
+} DxgiFormatMapping[] {
+#define _x(name) {#name, 0},
+#define _u(name, format) {nullptr, UnsignedInt(PixelFormat::format)},
+#include "DxgiFormat.h"
+#undef _u
+#undef _x
+};
 
 }
 
@@ -467,8 +281,6 @@ void DdsImporter::doOpenData(Containers::Array<char>&& data, const DataFlags dat
                 f->pixelFormat.compressed = CompressedPixelFormat::Bc3RGBAUnorm;
                 break;
             case Utility::Endianness::fourCC('D', 'X', '1', '0'): {
-                f->compressed = false;
-                f->needsSwizzle = false;
                 if(f->in.exceptPrefix(offset).size() < sizeof(DdsHeaderDxt10)) {
                     Error() << "Trade::DdsImporter::openData(): fourcc was DX10 but file is too short to contain DXT10 header";
                     return;
@@ -476,13 +288,21 @@ void DdsImporter::doOpenData(Containers::Array<char>&& data, const DataFlags dat
                 const DdsHeaderDxt10& dxt10 = *reinterpret_cast<const DdsHeaderDxt10*>(f->in.exceptPrefix(offset).data());
                 offset += sizeof(DdsHeaderDxt10);
 
-                f->pixelFormat.uncompressed = dxgiToGl(dxt10.dxgiFormat);
-                if(f->pixelFormat.uncompressed == PixelFormat(-1)) {
-                    Error() << "Trade::DdsImporter::openData(): unsupported DXGI format" << UnsignedInt(dxt10.dxgiFormat);
+                if(dxt10.dxgiFormat >= Containers::arraySize(DxgiFormatMapping)) {
+                    Error{} << "Trade::DdsImporter::openData(): unknown DXGI format ID" << dxt10.dxgiFormat;
+                    return;
+                }
+
+                const auto& mapped = DxgiFormatMapping[dxt10.dxgiFormat];
+                if(mapped.format) {
+                    f->compressed = false;
+                    f->needsSwizzle = false;
+                    f->pixelFormat.uncompressed = PixelFormat(mapped.format);
+                } else {
+                    Error{} << "Trade::DdsImporter::openData(): unsupported format DXGI_FORMAT_" << Debug::nospace << mapped.name;
                     return;
                 }
             } break;
-
             case Utility::Endianness::fourCC('D', 'X', 'T', '2'):
             case Utility::Endianness::fourCC('D', 'X', 'T', '4'):
             default:
---
+++ src/MagnumPlugins/DdsImporter/DxgiFormat.h
@@ -0,0 +1,126 @@
+/*
+    _x() -- skipped (not supported)
+    _u() -- uncompressed format
+*/
+#ifdef _u
+_x(UNKNOWN)
+_u(R32G32B32A32_TYPELESS,   RGBA32UI)   /* in Magnum, UI == typeless */
+_u(R32G32B32A32_FLOAT,      RGBA32F)
+_u(R32G32B32A32_UINT,       RGBA32UI)
+_u(R32G32B32A32_SINT,       RGBA32I)
+_u(R32G32B32_TYPELESS,      RGB32UI)    /* in Magnum, UI == typeless */
+_u(R32G32B32_FLOAT,         RGB32F)
+_u(R32G32B32_UINT,          RGB32UI)
+_u(R32G32B32_SINT,          RGB32I)
+_u(R16G16B16A16_TYPELESS,   RGBA16UI)   /* in Magnum, UI == typeless */
+_u(R16G16B16A16_FLOAT,      RGBA16F)
+_u(R16G16B16A16_UNORM,      RGBA16Unorm)
+_u(R16G16B16A16_UINT,       RGBA16UI)
+_u(R16G16B16A16_SNORM,      RGBA16Snorm)
+_u(R16G16B16A16_SINT,       RGBA16I)
+_u(R32G32_TYPELESS,         RG32UI)     /* in Magnum, UI == typeless */
+_u(R32G32_FLOAT,            RG32F)
+_u(R32G32_UINT,             RG32UI)
+_u(R32G32_SINT,             RG32I)
+_x(R32G8X24_TYPELESS)
+_x(D32_FLOAT_S8X24_UINT)
+_x(R32_FLOAT_X8X24_TYPELESS)
+_x(X32_TYPELESS_G8X24_UINT)
+_x(R10G10B10A2_TYPELESS)    /* no generic packed formats in Magnum yet */
+_x(R10G10B10A2_UNORM)
+_x(R10G10B10A2_UINT)
+_x(R11G11B10_FLOAT)
+_u(R8G8B8A8_TYPELESS,       RGBA8UI)    /* in Magnum, UI == typeless */
+_u(R8G8B8A8_UNORM,          RGBA8Unorm)
+_u(R8G8B8A8_UNORM_SRGB,     RGBA8Unorm) /** @todo propagate sRGB property */
+_u(R8G8B8A8_UINT,           RGBA8UI)
+_u(R8G8B8A8_SNORM,          RGBA8Snorm)
+_u(R8G8B8A8_SINT,           RGBA8I)
+_u(R16G16_TYPELESS,         RG16UI)     /* in Magnum, UI == typeless */
+_u(R16G16_FLOAT,            RG16F)
+_u(R16G16_UNORM,            RG16Unorm)
+_u(R16G16_UINT,             RG16UI)
+_u(R16G16_SNORM,            RG16Snorm)
+_u(R16G16_SINT,             RG16I)
+_u(R32_TYPELESS,            R32UI)      /* in Magnum, UI == typeless */
+_x(D32_FLOAT)
+_u(R32_FLOAT,               R32F)
+_u(R32_UINT,                R32UI)
+_u(R32_SINT,                R32I)
+_x(R24G8_TYPELESS)
+_x(D24_UNORM_S8_UINT)
+_x(R24_UNORM_X8_TYPELESS)
+_x(X24_TYPELESS_G8_UINT)
+_u(R8G8_TYPELESS,           RG8UI)      /* in Magnum, UI == typeless */
+_u(R8G8_UNORM,              RG8Unorm)
+_u(R8G8_UINT,               RG8UI)
+_u(R8G8_SNORM,              RG8Snorm)
+_u(R8G8_SINT,               RG8I)
+_u(R16_TYPELESS,            R16UI)      /* in Magnum, UI == typeless */
+_u(R16_FLOAT,               R16F)
+_x(D16_UNORM)
+_u(R16_UNORM,               R16Unorm)
+_u(R16_UINT,                R16UI)
+_u(R16_SNORM,               R16Snorm)
+_u(R16_SINT,                R16I)
+_u(R8_TYPELESS,             R8UI)       /* in Magnum, UI == typeless */
+_u(R8_UNORM,                R8Unorm)
+_u(R8_UINT,                 R8UI)
+_u(R8_SNORM,                R8Snorm)
+_u(R8_SINT,                 R8I)
+_u(A8_UNORM,                R8Unorm)    /* only R as a single-channel format */
+_x(R1_UNORM)                /* no single-bit formats in Magnum */
+_x(R9G9B9E5_SHAREDEXP)      /* no generic packed formats in Magnum yet */
+_x(R8G8_B8G8_UNORM)         /* no YUV formats in Magnum yet */
+_x(G8R8_G8B8_UNORM)
+_x(BC1_TYPELESS)
+_x(BC1_UNORM)
+_x(BC1_UNORM_SRGB)
+_x(BC2_TYPELESS)
+_x(BC2_UNORM)
+_x(BC2_UNORM_SRGB)
+_x(BC3_TYPELESS)
+_x(BC3_UNORM)
+_x(BC3_UNORM_SRGB)
+_x(BC4_TYPELESS)
+_x(BC4_UNORM)
+_x(BC4_SNORM)
+_x(BC5_TYPELESS)
+_x(BC5_UNORM)
+_x(BC5_SNORM)
+_x(B5G6R5_UNORM)            /* no generic packed formats in Magnum yet */
+_x(B5G5R5A1_UNORM)
+_x(B8G8R8A8_UNORM)
+_x(B8G8R8X8_UNORM)
+_x(R10G10B10_XR_BIAS_A2_UNORM) /* no XR formats in Magnum yet */
+_x(B8G8R8A8_TYPELESS)
+_x(B8G8R8A8_UNORM_SRGB)
+_x(B8G8R8X8_TYPELESS)
+_x(B8G8R8X8_UNORM_SRGB)
+_x(BC6H_TYPELESS)
+_x(BC6H_UF16)
+_x(BC6H_SF16)
+_x(BC7_TYPELESS)
+_x(BC7_UNORM)
+_x(BC7_UNORM_SRGB)
+_x(AYUV)                    /* no YUV formats in Magnum yet */
+_x(Y410)
+_x(Y416)
+_x(NV12)
+_x(P010)                    /* no (planar) YUV formats in Magnum yet */
+_x(P016)
+_x(420_OPAQUE)              /* no YUV formats in Magnum yet */
+_x(YUY2)
+_x(Y210)
+_x(Y216)
+_x(NV11)
+_x(AI44)
+_x(IA44)
+_x(P8)                      /* no (planar) YUV formats in Magnum yet */
+_x(A8P8)
+_x(B4G4R4A4_UNORM)          /* no generic packed formats in Magnum yet */
+/* DXGI_FORMAT_P208 is not linearly numbered, skipping */
+/* DXGI_FORMAT_V208 is not linearly numbered, skipping */
+/* DXGI_FORMAT_V408 is not linearly numbered, skipping */
+/* DXGI_FORMAT_FORCE_UINT is just an "expander", skipping */
+#endif
