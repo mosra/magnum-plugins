e9c41afc49fcb33627b776522f5a014b160cf854
bowling-allie <allie.smith.epic@gmail.com>
2020-02-11 13:01:51 -0600

    New plugin: IcoImporter

---
+++ src/MagnumPlugins/IcoImporter/IcoImporter.cpp
@@ -0,0 +1,256 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
+              Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "IcoImporter.h"
+
+#include <algorithm>
+
+#include <Corrade/Containers/Optional.h>
+#include <Corrade/Utility/Debug.h>
+#include <Corrade/Utility/Endianness.h>
+#include <Magnum/Trade/ImageData.h>
+
+namespace Magnum { namespace Trade {
+
+namespace {
+/*
+void deconstructIntToLittleEndian(
+    UnsignedInt in,
+    char *outByte0,
+    char *outByte1,
+    char *outByte2,
+    char *outByte3
+) {
+    Utility::Endianness::littleEndianInPlace(in);
+    *outByte0 = static_cast<char>(in);
+    *outByte1 = static_cast<char>(in >> 8u);
+    *outByte2 = static_cast<char>(in >> 16u);
+    *outByte3 = static_cast<char>(in >> 24u);
+}
+*/
+
+/*
+struct BitmapInfoHeader {
+    static constexpr std::size_t Size = 40;
+    enum class CompressionMethod : UnsignedInt {
+        BI_RGB = 0,
+        BI_RLE8 = 1,
+        BI_RLE4 = 2,
+        BI_BITFIELDS = 3,
+        BI_JPEG = 4,
+        BI_PNG = 5,
+        BI_ALPHABITFIELDS = 6,
+        BI_CMYK = 11,
+        BI_CMYKRLE8 = 12,
+        BI_CMYKRLE4 = 13
+    };
+
+    UnsignedInt headerSize;
+    Int width;
+    Int height;
+    UnsignedShort colorPlanes; // must be 1
+    UnsignedShort bitsPerPixel; // Typical values are 1, 4, 8, 16, 24 and 32
+    CompressionMethod compressionMethod;
+    UnsignedInt imageDataSize;
+    Int horzontalDensity; // pixel per metre
+    Int verticalDensity; // pixel per metre
+    UnsignedInt colorCount; // the number of colors in the color palette, or 0 to default to 2^n
+    UnsignedInt importantColors; // the number of important colors used, or 0 when every color is important; generally ignored
+};
+static_assert(sizeof(BitmapInfoHeader) == BitmapInfoHeader::Size, "sizeof(BitmapInfoHeader) is incorrect");
+*/
+
+struct IconDir {
+    static constexpr std::size_t Size = 6;
+    UnsignedShort reserved; // must be 0
+    UnsignedShort imageType; // must be either 1 for .ICO, or 2 for .CUR
+    UnsignedShort imageCount;
+};
+static_assert(sizeof(IconDir) == IconDir::Size, "sizeof(IconDir) is incorrect");
+
+struct IconDirEntry {
+    static constexpr std::size_t Size = 16;
+    UnsignedByte imageWidth; // pixels. Value 0 means image width is 256 pixels. :[
+    UnsignedByte imageHeight; // pixels. Value 0 means image width is 256 pixels. :[
+    UnsignedByte colorCount;
+    UnsignedByte reserved; // must be 0
+    union {
+        UnsignedShort colorPlanes;
+        UnsignedShort hotspotX;
+    };
+    union {
+        UnsignedShort bitsPerPixel;
+        UnsignedShort hotspotY;
+    };
+    UnsignedInt imageDataSize;
+    UnsignedInt imageDataOffset;
+};
+static_assert(sizeof(IconDirEntry) == IconDirEntry::Size, "sizeof(IconDirEntry) is incorrect");
+
+}
+
+IcoImporter::IcoImporter() = default;
+
+IcoImporter::IcoImporter(PluginManager::AbstractManager& manager, const std::string& plugin) : AbstractImporter{manager, plugin} {}
+
+IcoImporter::~IcoImporter() = default;
+
+auto IcoImporter::doFeatures() const -> Features { return Feature::OpenData; }
+
+bool IcoImporter::doIsOpened() const { return _isOpened; }
+
+void IcoImporter::doClose() {
+    _isOpened = false;
+    _imageDataArray = nullptr;
+}
+
+namespace {
+    constexpr std::initializer_list<const char> bmpFileHeaderTemplate{'B', 'M', 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    constexpr std::initializer_list<const char> pngMagicNum{static_cast<char>(0x89), 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+}
+
+void IcoImporter::doOpenData(const Containers::ArrayView<const char> data) {
+    _isOpened = false;
+    /* Because here we're copying the data and using the _in to check if file
+       is opened, having them nullptr would mean openData() would fail without
+       any error message. It's not possible to do this check on the importer
+       side, because empty file is valid in some formats (OBJ or glTF). We also
+       can't do the full import here because then doImage2D() would need to
+       copy the imported data instead anyway (and the uncompressed size is much
+       larger). This way it'll also work nicely with a future openMemory(). */
+    if(data.empty()) {
+        Error{} << "Trade::IcoImporter::openData(): the file is empty";
+        return;
+    }
+
+    CORRADE_ASSERT(data.size() >= 6,
+                   "Trade::IcoImporter::image2D(): header too short", );
+    IconDir header = *reinterpret_cast<const IconDir*>(data.begin());
+    Utility::Endianness::littleEndianInPlace(
+        header.reserved,
+        header.imageType,
+        header.imageCount
+    );
+    CORRADE_ASSERT(header.reserved == 0,
+                   "Trade::IcoImporter::image2D(): reserved data incorrect", );
+
+    _imageType = static_cast<ImageType>(header.imageType);
+    _imageCount = header.imageCount;
+
+    _imageDataArray = Containers::Array<std::pair<bool, Containers::Array<char>>>{_imageCount};
+
+    for (Int i = 0; i < header.imageCount; ++i) {
+        Int iconDirEntryOffset = IconDir::Size + (IconDirEntry::Size * i);
+        CORRADE_ASSERT(data.size() >= (iconDirEntryOffset + IconDirEntry::Size),
+                       "Trade::IcoImporter::image2D(): image entry header too short", );
+
+        IconDirEntry iconDirEntry = *reinterpret_cast<const IconDirEntry*>(data.begin() + iconDirEntryOffset);
+        Utility::Endianness::littleEndianInPlace(
+            iconDirEntry.imageWidth,
+            iconDirEntry.imageHeight,
+            iconDirEntry.colorCount,
+            iconDirEntry.reserved,
+            iconDirEntry.colorPlanes,
+            iconDirEntry.bitsPerPixel,
+            iconDirEntry.imageDataSize,
+            iconDirEntry.imageDataOffset
+        );
+        CORRADE_ASSERT(iconDirEntry.reserved == 0,
+                       "Trade::IcoImporter::image2D(): reserved data incorrect", );
+
+        const char* imageDataBegin = data.begin() + iconDirEntry.imageDataOffset;
+        const char* imageDataEnd = imageDataBegin + iconDirEntry.imageDataSize;
+        CORRADE_ASSERT(data.size() >= (iconDirEntry.imageDataOffset + iconDirEntry.imageDataSize),
+                       "Trade::IcoImporter::image2D(): reserved bytes incorrect", );
+
+        bool isPng = std::equal(pngMagicNum.begin(), pngMagicNum.end(), imageDataBegin);
+        auto &imageData = std::get<1>(_imageDataArray[i]);
+        if (isPng) {
+            imageData = Containers::Array<char>{Containers::NoInit, iconDirEntry.imageDataSize};
+            std::copy(imageDataBegin, imageDataEnd, imageData.begin());
+        } else {
+            /* BMP data in ICO files isn't valid :( */
+            /*
+            UnsignedInt headerSize = *reinterpret_cast<const UnsignedInt*>(imageDataBegin);
+            if (headerSize == 40) {
+                imageData = Containers::Array<char>{iconDirEntry.imageDataSize + bmpFileHeaderTemplate.size()};
+                std::copy(bmpFileHeaderTemplate.begin(), bmpFileHeaderTemplate.end(), imageData.begin());
+                deconstructIntToLittleEndian(
+                    iconDirEntry.imageDataSize + bmpFileHeaderTemplate.size(),
+                    &imageData[2],
+                    &imageData[3],
+                    &imageData[4],
+                    &imageData[5]
+                );
+                UnsignedInt headerSize = *reinterpret_cast<const UnsignedInt*>(imageDataBegin);
+                Utility::Endianness::littleEndianInPlace(headerSize);
+                deconstructIntToLittleEndian(
+                    headerSize + bmpFileHeaderTemplate.size(),
+                    &imageData[10],
+                    &imageData[11],
+                    &imageData[12],
+                    &imageData[13]
+                );
+                std::copy(imageDataBegin, imageDataEnd, imageData.begin() + bmpFileHeaderTemplate.size());
+            } else {
+                imageData = Containers::Array<char>{};
+            }
+            */
+            imageData = Containers::Array<char>{};
+        }
+
+        std::get<0>(_imageDataArray[i]) = isPng;
+    }
+
+    _isOpened = true;
+}
+
+UnsignedInt IcoImporter::doImage2DCount() const { return _imageCount; }
+
+Containers::Optional<ImageData2D> IcoImporter::doImage2D(UnsignedInt id) {
+    bool isPng = std::get<0>(_imageDataArray[id]);
+    if (isPng) {
+        /* just delegate actual image importing */
+        if (!_pngImporter) {
+            _pngImporter = manager()->loadAndInstantiate("PngImporter");
+        }
+        CORRADE_ASSERT(_pngImporter,
+                       "Trade::IcoImporter::image2D(): the correct image importer is unavailable", Containers::NullOpt);
+
+        if (!_pngImporter->openData(std::get<1>(_imageDataArray[id]))) {
+            return Containers::NullOpt;
+        }
+
+        return _pngImporter->image2D(0);
+    }
+
+    /* sadly, BMP data in ICO files isn't valid, as it contains transparency. and so, we parse */
+    return Containers::NullOpt;
+}
+
+}}
+
+CORRADE_PLUGIN_REGISTER(IcoImporter, Magnum::Trade::IcoImporter,
+    "cz.mosra.magnum.Trade.AbstractImporter/0.3")
---
+++ src/MagnumPlugins/IcoImporter/Test/IcoImporterTest.cpp
@@ -0,0 +1,131 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
+              Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include <sstream>
+#include <Corrade/Containers/Optional.h>
+#include <Corrade/TestSuite/Tester.h>
+#include <Corrade/TestSuite/Compare/Container.h>
+#include <Corrade/Utility/Directory.h>
+#include <Corrade/Utility/DebugStl.h>
+#include <Corrade/Utility/FormatStl.h>
+#include <Magnum/PixelFormat.h>
+#include <Magnum/Trade/AbstractImporter.h>
+#include <Magnum/Trade/ImageData.h>
+
+#include "configure.h"
+
+namespace Magnum { namespace Trade { namespace Test { namespace {
+
+struct IcoImporterTest: TestSuite::Tester {
+    explicit IcoImporterTest();
+
+    //void bmp_1bpp_1ba_2sp();
+    //void bmp_4bpp_1ba_16sp();
+    //void bmp_8bpp_1ba_256sp();
+    //void bmp_24bpp_1ba_np();
+    void bmp_32bpp_8ba_np();
+    void png();
+
+    /* Explicitly forbid system-wide plugin dependencies */
+    PluginManager::Manager<AbstractImporter> _manager{"nonexistent"};
+};
+
+IcoImporterTest::IcoImporterTest() {
+
+    addTests({
+         //&IcoImporterTest::bmp_1bpp_1ba_2sp,
+         //&IcoImporterTest::bmp_4bpp_1ba_16sp,
+         //&IcoImporterTest::bmp_8bpp_1ba_256sp,
+         //&IcoImporterTest::bmp_24bpp_1ba_np,
+         &IcoImporterTest::bmp_32bpp_8ba_np,
+         &IcoImporterTest::png
+    });
+
+    /* Load the plugin directly from the build tree. Otherwise it's static and
+       already loaded. */
+    #ifdef ICOIMPORTER_PLUGIN_FILENAME
+    CORRADE_INTERNAL_ASSERT(_manager.load(ICOIMPORTER_PLUGIN_FILENAME) & PluginManager::LoadState::Loaded);
+    #endif
+}
+/*
+void IcoImporterTest::bmp_1bpp_1ba_2sp() {
+    Containers::Pointer<AbstractImporter> importer = _manager.instantiate("IcoImporter");
+    CORRADE_VERIFY(importer->openFile(Utility::Directory::join(ICOIMPORTER_TEST_DIR, "stone.ico")));
+
+    Containers::Optional<Trade::ImageData2D> image = importer->image2D(0);
+    CORRADE_VERIFY(image);
+    CORRADE_COMPARE(image->size(), (Vector2i{8, 8}));
+}
+
+void IcoImporterTest::bmp_4bpp_1ba_16sp() {
+    Containers::Pointer<AbstractImporter> importer = _manager.instantiate("IcoImporter");
+    CORRADE_VERIFY(importer->openFile(Utility::Directory::join(ICOIMPORTER_TEST_DIR, "stone.ico")));
+
+    Containers::Optional<Trade::ImageData2D> image = importer->image2D(1);
+    CORRADE_VERIFY(image);
+    CORRADE_COMPARE(image->size(), (Vector2i{16, 16}));
+}
+
+void IcoImporterTest::bmp_8bpp_1ba_256sp() {
+    Containers::Pointer<AbstractImporter> importer = _manager.instantiate("IcoImporter");
+    CORRADE_VERIFY(importer->openFile(Utility::Directory::join(ICOIMPORTER_TEST_DIR, "stone.ico")));
+
+    Containers::Optional<Trade::ImageData2D> image = importer->image2D(2);
+    CORRADE_VERIFY(image);
+    CORRADE_COMPARE(image->size(), (Vector2i{32, 32}));
+}
+
+void IcoImporterTest::bmp_24bpp_1ba_np() {
+    Containers::Pointer<AbstractImporter> importer = _manager.instantiate("IcoImporter");
+    CORRADE_VERIFY(importer->openFile(Utility::Directory::join(ICOIMPORTER_TEST_DIR, "stone.ico")));
+
+    Containers::Optional<Trade::ImageData2D> image = importer->image2D(3);
+    CORRADE_VERIFY(image);
+    CORRADE_COMPARE(image->size(), (Vector2i{64, 64}));
+}
+*/
+void IcoImporterTest::bmp_32bpp_8ba_np() {
+    Containers::Pointer<AbstractImporter> importer = _manager.instantiate("IcoImporter");
+    CORRADE_VERIFY(importer->openFile(Utility::Directory::join(ICOIMPORTER_TEST_DIR, "stone.ico")));
+
+    Containers::Optional<Trade::ImageData2D> image = importer->image2D(4);
+
+    CORRADE_EXPECT_FAIL("IcoImporter does not support BMPs yet.");
+    CORRADE_VERIFY(image);
+    //CORRADE_COMPARE(image->size(), (Vector2i{128, 128}));
+}
+
+void IcoImporterTest::png() {
+    Containers::Pointer<AbstractImporter> importer = _manager.instantiate("IcoImporter");
+    CORRADE_VERIFY(importer->openFile(Utility::Directory::join(ICOIMPORTER_TEST_DIR, "stone.ico")));
+
+    Containers::Optional<Trade::ImageData2D> image = importer->image2D(5);
+    CORRADE_VERIFY(image);
+    CORRADE_COMPARE(image->size(), (Vector2i{256, 256}));
+}
+
+}}}}
+
+CORRADE_TEST_MAIN(Magnum::Trade::Test::IcoImporterTest)
