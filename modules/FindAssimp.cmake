591ccb967f740a6965ccdf51006d911273451f60
Vladimír Vondruš <mosra@centrum.cz>
2020-11-04 11:09:27 +0100

    modules: update naming in FindAssimp internals to follow CMake rules.
    Shouldn't cause any breakage for the users, as those variables are meant
    to be internal for users. For forcing Assimp location there are now
    aliases to the old names.

--- modules/FindAssimp.cmake
+++ modules/FindAssimp.cmake
@@ -9,8 +9,8 @@
 #
 # Additionally these variables are defined for internal usage:
 #
-#  ASSIMP_LIBRARY         - Assimp library
-#  ASSIMP_INCLUDE_DIR     - Include dir
+#  Assimp_LIBRARY         - Assimp library
+#  Assimp_INCLUDE_DIR     - Include dir
 #
 
 #
@@ -39,7 +39,11 @@
 #   DEALINGS IN THE SOFTWARE.
 #
 
-find_path(ASSIMP_INCLUDE_DIR NAMES assimp/anim.h HINTS include)
+find_path(Assimp_INCLUDE_DIR NAMES assimp/anim.h
+    HINTS
+        include
+        # For backwards compatibility
+        ${ASSIMP_INCLUDE_DIR})
 
 if(WIN32 AND MSVC)
     # Adapted from https://github.com/assimp/assimp/blob/799fd74714f9ffac29004c6b5a674b3402524094/CMakeLists.txt#L645-L655
@@ -47,69 +51,75 @@ if(WIN32 AND MSVC)
     # use of MSVCxy replaced with MSVC_VERSION. See also
     # https://en.wikipedia.org/wiki/Microsoft_Visual_C%2B%2B#Internal_version_numbering
     if(MSVC_TOOLSET_VERSION) # available only since CMake 3.12
-        set(ASSIMP_MSVC_VERSION vc${MSVC_TOOLSET_VERSION})
+        set(Assimp_MSVC_VERSION vc${MSVC_TOOLSET_VERSION})
     elseif(MSVC_VERSION VERSION_LESS 1910)
-        set(ASSIMP_MSVC_VERSION vc140)
+        set(Assimp_MSVC_VERSION vc140)
     elseif(MSVC_VERSION VERSION_LESS 1920)
-        set(ASSIMP_MSVC_VERSION vc141)
+        set(Assimp_MSVC_VERSION vc141)
     elseif(MSVC_VERSION VERSION_LESS 1930)
-        set(ASSIMP_MSVC_VERSION vc142)
+        set(Assimp_MSVC_VERSION vc142)
     else()
         message(FATAL_ERROR "Unsupported MSVC version")
     endif()
 
     if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-        set(ASSIMP_LIBRARY_DIR "lib64")
+        set(Assimp_LIBRARY_DIR "lib64")
     elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
-        set(ASSIMP_LIBRARY_DIR "lib32")
+        set(Assimp_LIBRARY_DIR "lib32")
     endif()
 
-    find_library(ASSIMP_LIBRARY_RELEASE assimp-${ASSIMP_MSVC_VERSION}-mt.lib PATHS ${ASSIMP_LIBRARY_DIR})
-    find_library(ASSIMP_LIBRARY_DEBUG assimp-${ASSIMP_MSVC_VERSION}-mtd.lib PATHS ${ASSIMP_LIBRARY_DIR})
+    find_library(Assimp_LIBRARY_RELEASE assimp-${Assimp_MSVC_VERSION}-mt.lib
+        PATHS ${Assimp_LIBRARY_DIR}
+        # For backwards compatibility
+        HINTS ${ASSIMP_LIBRARY_RELEASE})
+    find_library(Assimp_LIBRARY_DEBUG assimp-${Assimp_MSVC_VERSION}-mtd.lib
+        PATHS ${Assimp_LIBRARY_DIR}
+        # For backwards compatibility
+        HINTS ${ASSIMP_LIBRARY_DEBUG})
 else()
-    find_library(ASSIMP_LIBRARY_RELEASE assimp)
-    find_library(ASSIMP_LIBRARY_DEBUG assimpd)
+    find_library(Assimp_LIBRARY_RELEASE assimp)
+    find_library(Assimp_LIBRARY_DEBUG assimpd)
 endif()
 
 # Static build of Assimp (built with Vcpkg, any system) depends on IrrXML, find
 # that one as well. If not found, simply don't link to it --- it might be a
 # dynamic build (on Windows it's a *.lib either way), or a static build using
 # system IrrXML. Related issue: https://github.com/Microsoft/vcpkg/issues/5012
-if(ASSIMP_LIBRARY_DEBUG MATCHES "${CMAKE_STATIC_LIBRARY_SUFFIX}$" OR ASSIMP_LIBRARY_RELEASE MATCHES "${CMAKE_STATIC_LIBRARY_SUFFIX}$")
-    find_library(ASSIMP_IRRXML_LIBRARY_RELEASE IrrXML)
-    find_library(ASSIMP_IRRXML_LIBRARY_DEBUG IrrXMLd)
+if(Assimp_LIBRARY_DEBUG MATCHES "${CMAKE_STATIC_LIBRARY_SUFFIX}$" OR Assimp_LIBRARY_RELEASE MATCHES "${CMAKE_STATIC_LIBRARY_SUFFIX}$")
+    find_library(Assimp_IRRXML_LIBRARY_RELEASE IrrXML)
+    find_library(Assimp_IRRXML_LIBRARY_DEBUG IrrXMLd)
 endif()
 
 include(SelectLibraryConfigurations)
-select_library_configurations(ASSIMP)
+select_library_configurations(Assimp)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Assimp DEFAULT_MSG
-    ASSIMP_LIBRARY
-    ASSIMP_INCLUDE_DIR)
+    Assimp_LIBRARY
+    Assimp_INCLUDE_DIR)
 
 if(NOT TARGET Assimp::Assimp)
     add_library(Assimp::Assimp UNKNOWN IMPORTED)
 
-    if(ASSIMP_LIBRARY_DEBUG AND ASSIMP_LIBRARY_RELEASE)
+    if(Assimp_LIBRARY_DEBUG AND Assimp_LIBRARY_RELEASE)
         set_target_properties(Assimp::Assimp PROPERTIES
-            IMPORTED_LOCATION_DEBUG ${ASSIMP_LIBRARY_DEBUG}
-            IMPORTED_LOCATION_RELEASE ${ASSIMP_LIBRARY_RELEASE})
+            IMPORTED_LOCATION_DEBUG ${Assimp_LIBRARY_DEBUG}
+            IMPORTED_LOCATION_RELEASE ${Assimp_LIBRARY_RELEASE})
     else()
         set_target_properties(Assimp::Assimp PROPERTIES
-            IMPORTED_LOCATION ${ASSIMP_LIBRARY})
+            IMPORTED_LOCATION ${Assimp_LIBRARY})
     endif()
 
     # Link to IrrXML as well, if found. See the comment above for details.
-    if(ASSIMP_IRRXML_LIBRARY_RELEASE)
+    if(Assimp_IRRXML_LIBRARY_RELEASE)
         set_property(TARGET Assimp::Assimp APPEND PROPERTY
-            INTERFACE_LINK_LIBRARIES $<$<NOT:$<CONFIG:Debug>>:${ASSIMP_IRRXML_LIBRARY_RELEASE}>)
+            INTERFACE_LINK_LIBRARIES $<$<NOT:$<CONFIG:Debug>>:${Assimp_IRRXML_LIBRARY_RELEASE}>)
     endif()
-    if(ASSIMP_IRRXML_LIBRARY_DEBUG)
+    if(Assimp_IRRXML_LIBRARY_DEBUG)
         set_property(TARGET Assimp::Assimp APPEND PROPERTY
-            INTERFACE_LINK_LIBRARIES $<$<CONFIG:Debug>:${ASSIMP_IRRXML_LIBRARY_DEBUG}>)
+            INTERFACE_LINK_LIBRARIES $<$<CONFIG:Debug>:${Assimp_IRRXML_LIBRARY_DEBUG}>)
     endif()
 
     set_target_properties(Assimp::Assimp PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES ${ASSIMP_INCLUDE_DIR})
+        INTERFACE_INCLUDE_DIRECTORIES ${Assimp_INCLUDE_DIR})
 endif()
