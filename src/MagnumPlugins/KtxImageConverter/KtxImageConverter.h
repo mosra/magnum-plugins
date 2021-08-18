5d584587864d6c670b94e6a7b1a6436d67e85fa2
Pablo Escobar <mail@rvrs.in>
2021-08-18 23:58:19 +0200

    KtxImageConverter: docs++

--- src/MagnumPlugins/KtxImageConverter/KtxImageConverter.h
+++ src/MagnumPlugins/KtxImageConverter/KtxImageConverter.h
@@ -26,6 +26,11 @@
     DEALINGS IN THE SOFTWARE.
 */
 
+/** @file
+ * @brief Class @ref Magnum::Trade::KtxImageConverter
+ * @m_since_latest_{plugins}
+ */
+
 #include <Magnum/Trade/AbstractImageConverter.h>
 
 #include "MagnumPlugins/KtxImageConverter/configure.h"
@@ -48,6 +53,76 @@
 
 namespace Magnum { namespace Trade {
 
+/**
+@brief KTX2 image converter plugin
+@m_since_latest_{plugins}
+
+Creates Khronos Texture 2.0 (`*.ktx2`) files from images. You can use
+@ref KtxImporter to import images in this format.
+
+@section Trade-KtxImageConverter-usage Usage
+
+This plugin depends on the @ref Trade library and is built if
+`WITH_KTXIMAGECONVERTER` is enabled when building Magnum Plugins. To use as
+a dynamic plugin, load @cpp "KtxImageConverter" @ce via
+@ref Corrade::PluginManager::Manager.
+
+Additionally, if you're using Magnum as a CMake subproject, bundle the
+[magnum-plugins](https://github.com/mosra/magnum-plugins) and do the following:
+
+@code{.cmake}
+set(WITH_KTXIMAGECONVERTER ON CACHE BOOL "" FORCE)
+add_subdirectory(magnum-plugins EXCLUDE_FROM_ALL)
+
+# So the dynamically loaded plugin gets built implicitly
+add_dependencies(your-app MagnumPlugins::KtxImageConverter)
+@endcode
+
+To use as a static plugin or as a dependency of another plugin with CMake, put
+[FindMagnumPlugins.cmake](https://github.com/mosra/magnum-plugins/blob/master/modules/FindMagnumPlugins.cmake)
+into your `modules/` directory, request the `KtxImageConverter` component
+of the `MagnumPlugins` package and link to the
+`MagnumPlugins::KtxImageConverter` target:
+
+@code{.cmake}
+find_package(MagnumPlugins REQUIRED KtxImageConverter)
+
+# ...
+target_link_libraries(your-app PRIVATE MagnumPlugins::KtxImageConverter)
+@endcode
+
+See @ref building-plugins, @ref cmake-plugins, @ref plugins and
+@ref file-formats for more information.
+
+@section Trade-KtxImageConverter-behavior Behavior and limitations
+
+@subsection Trade-KtxImageConverter-behavior-formats Supported formats
+
+The following formats can be written:
+
+-   all formats in @ref PixelFormat
+-   all formats in @ref CompressedPixelFormat, except for 3D ASTC formats
+
+@subsection Trade-KtxImageConverter-behavior-cube-array Cube map and array images
+
+Cube map and array images can be written but there is currently no way to mark
+them properly in the metadata. Exported files will be 3D images with cube map
+faces and array layers exposed as depth slices.
+
+@subsection Trade-KtxImageConverter-behavior-supercompression Supercompression
+
+Supercompression is not supported.
+
+@section Trade-KtxImageConverter-configuration Plugin-specific configuration
+
+It's possible to tune various metadata options through @ref configuration().
+See below for all options and their default values:
+
+@snippet MagnumPlugins/KtxImageConverter/KtxImageConverter.conf config
+
+See @ref plugins-configuration for more information and an example showing how
+to edit the configuration values.
+*/
 class MAGNUM_KTXIMAGECONVERTER_EXPORT KtxImageConverter: public AbstractImageConverter {
     public:
         /** @brief Plugin manager constructor */
