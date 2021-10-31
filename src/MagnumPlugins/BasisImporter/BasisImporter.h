94adcec97025ca8d1c25ccfba5a8118f0a9ccc9e
Vladimír Vondruš <mosra@centrum.cz>
2021-10-31 18:02:53 +0100

    BasisImporter: doc++

--- src/MagnumPlugins/BasisImporter/BasisImporter.h
+++ src/MagnumPlugins/BasisImporter/BasisImporter.h
@@ -215,12 +215,12 @@ OpenGL, OpenGL ES and WebGL extensions, in its full ugly glory:
 
 @snippet BasisImporter.cpp gl-extension-checks
 
-@subsection Trade-BasisImporter-compile-size Reducing compile size
+@subsection Trade-BasisImporter-binary-size Reducing binary size
 
 To reduce the binary size of the transcoder, Basis Universal supports a set of
 preprocessor defines to turn off unneeded features. The Basis Universal Wiki
 lists macros to disable specific [target formats](https://github.com/BinomialLLC/basis_universal/wiki/How-to-Use-and-Configure-the-Transcoder#shrinking-the-transcoders-compiled-size)
-as well as [KTX2 support](https://github.com/BinomialLLC/basis_universal/wiki/How-to-Use-and-Configure-the-Transcoder#disabling-ktx2-or-zstandard-usage).
+as well as [KTX2 support including the Zstd dependency](https://github.com/BinomialLLC/basis_universal/wiki/How-to-Use-and-Configure-the-Transcoder#disabling-ktx2-or-zstandard-usage).
 If you're building it from source with `BASIS_UNIVERSAL_DIR` set, add the
 desired defines before adding `magnum-plugins` as a subfolder:
 
