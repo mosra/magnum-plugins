15d1598a248258a61970d3d7aaac129beabf2320
Vladimír Vondruš <mosra@centrum.cz>
2021-11-13 16:08:59 +0100

    modules: recognize Zstd as a CMake subproject, support CMake configs.
    The CMake configs are useful to know if the static library needs to link
    to pthread -- if needed and not done, the BasisImporter plugin would
    fail to load due to a link error even though Basis itself doesn't use
    any of the multithreading APIs from zstd.

--- modules/FindZstd.cmake
+++ modules/FindZstd.cmake
@@ -38,6 +38,58 @@
 #   DEALINGS IN THE SOFTWARE.
 #
 
+# In case we have Zstd as a CMake subproject, a libzstd_shared / libzstd_static
+# target should be present. If not, try to find it via the installed config
+# file, as that is useful to know additional libraries to link to in case of a
+# static build.
+if(NOT TARGET libzstd_shared AND NOT TARGET libzstd_static)
+    find_package(zstd CONFIG QUIET)
+endif()
+
+# Unfortunately the CMake project itself doesn't provide namespaced ALIAS
+# targets so we have to do this insane branching here.
+set(_ZSTD_TARGET )
+set(_ZSTD_SUBPROJECT )
+if(TARGET libzstd_shared)
+    set(_ZSTD_TARGET libzstd_shared)
+    set(_ZSTD_SUBPROJECT ON)
+elseif(TARGET libzstd_static)
+    set(_ZSTD_TARGET libzstd_static)
+    set(_ZSTD_SUBPROJECT ON)
+elseif(TARGET zstd::libzstd_shared)
+    set(_ZSTD_TARGET zstd::libzstd_shared)
+elseif(TARGET zstd::libzstd_static)
+    set(_ZSTD_TARGET zstd::libzstd_static)
+endif()
+if(_ZSTD_TARGET)
+    if(NOT TARGET Zstd::Zstd)
+        # Aliases of (global) targets are only supported in CMake 3.11, so we
+        # work around it by this. This is easier than fetching all possible
+        # properties (which are impossible to track of) and then attempting to
+        # rebuild them into a new target.
+        add_library(Zstd::Zstd INTERFACE IMPORTED)
+        set_target_properties(Zstd::Zstd PROPERTIES
+            INTERFACE_LINK_LIBRARIES ${_ZSTD_TARGET})
+    endif()
+
+    if(_ZSTD_SUBPROJECT)
+        # The target is defined in build/cmake/lib/, includes are in lib/
+        get_target_property(_ZSTD_INTERFACE_INCLUDE_DIRECTORIES ${_ZSTD_TARGET} SOURCE_DIR)
+        get_filename_component(_ZSTD_INTERFACE_INCLUDE_DIRECTORIES ${_ZSTD_INTERFACE_INCLUDE_DIRECTORIES} DIRECTORY)
+        get_filename_component(_ZSTD_INTERFACE_INCLUDE_DIRECTORIES ${_ZSTD_INTERFACE_INCLUDE_DIRECTORIES} DIRECTORY)
+        get_filename_component(_ZSTD_INTERFACE_INCLUDE_DIRECTORIES ${_ZSTD_INTERFACE_INCLUDE_DIRECTORIES} DIRECTORY)
+        set(_ZSTD_INTERFACE_INCLUDE_DIRECTORIES ${_ZSTD_INTERFACE_INCLUDE_DIRECTORIES}/lib)
+    else()
+        get_target_property(_ZSTD_INTERFACE_INCLUDE_DIRECTORIES ${_ZSTD_TARGET} INTERFACE_INCLUDE_DIRECTORIES)
+    endif()
+
+    # Just to make FPHSA print some meaningful location, nothing else
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Zstd DEFAULT_MSG
+        _ZSTD_INTERFACE_INCLUDE_DIRECTORIES)
+    return()
+endif()
+
 # Library
 find_library(Zstd_LIBRARY NAMES zstd)
 
