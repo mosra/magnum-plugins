7abaea7ec7707ff47395471a8f186f415e59f5b5
Vladimír Vondruš <mosra@centrum.cz>
2022-02-14 10:40:52 +0100

    package/ci: add an iOS build on CircleCI.

---
+++ package/ci/ios-simulator.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+set -ev
+
+git submodule update --init
+
+# Corrade
+git clone --depth 1 https://github.com/mosra/corrade.git
+cd corrade
+
+# Build native corrade-rc
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+    -DWITH_INTERCONNECT=OFF \
+    -DWITH_PLUGINMANAGER=OFF \
+    -DWITH_TESTSUITE=OFF \
+    -DWITH_UTILITY=OFF \
+    -G Ninja
+ninja install
+cd ..
+
+# Crosscompile Corrade
+mkdir build-ios && cd build-ios
+cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DTESTSUITE_TARGET_XCTEST=ON \
+    -DWITH_INTERCONNECT=OFF \
+    -DBUILD_STATIC=ON \
+    -G Xcode
+set -o pipefail && cmake --build . --config Release --target install | xcbeautify
+cd ../..
+
+# Crosscompile Magnum
+git clone --depth 1 https://github.com/mosra/magnum.git
+cd magnum
+mkdir build-ios && cd build-ios
+cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DWITH_AUDIO=ON \
+    -DWITH_DEBUGTOOLS=ON \
+    -DWITH_GL=OFF \
+    -DWITH_MESHTOOLS=ON \
+    -DWITH_PRIMITIVES=ON \
+    -DWITH_SCENEGRAPH=OFF \
+    -DWITH_SCENETOOLS=OFF \
+    -DWITH_SHADERS=OFF \
+    -DWITH_TEXT=ON \
+    -DWITH_TEXTURETOOLS=ON \
+    -DWITH_ANYIMAGEIMPORTER=ON \
+    -DBUILD_STATIC=ON \
+    -G Xcode
+set -o pipefail && cmake --build . --config Release --target install | xcbeautify
+cd ../..
+
+# Crosscompile zstd
+export ZSTD_VERSION=1.5.0
+wget --no-check-certificate https://github.com/facebook/zstd/archive/refs/tags/v$ZSTD_VERSION.tar.gz
+tar -xzf v$ZSTD_VERSION.tar.gz
+cd zstd-$ZSTD_VERSION
+# There's already a directory named `build`
+mkdir build_ && cd build_
+cmake ../build/cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DZSTD_BUILD_PROGRAMS=OFF \
+    -DZSTD_BUILD_SHARED=OFF \
+    -DZSTD_BUILD_STATIC=ON \
+    -DZSTD_MULTITHREAD_SUPPORT=OFF \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -G Ninja
+ninja install
+cd ../..
+
+# Crosscompile. BUILD_GL_TESTS is enabled just to be sure, it should not be
+# needed by any plugin.
+mkdir build-ios && cd build-ios
+cmake .. \
+    -DCMAKE_TOOLCHAIN_FILE=../toolchains/generic/iOS.cmake \
+    -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk \
+    -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DWITH_ASSIMPIMPORTER=OFF \
+    -DWITH_BASISIMAGECONVERTER=OFF \
+    -DWITH_BASISIMPORTER=ON -DBASIS_UNIVERSAL_DIR=$HOME/basis_universal \
+    -DWITH_CGLTFIMPORTER=ON \
+    -DWITH_DDSIMPORTER=ON \
+    -DWITH_DEVILIMAGEIMPORTER=OFF \
+    -DWITH_DRFLACAUDIOIMPORTER=ON \
+    -DWITH_DRMP3AUDIOIMPORTER=ON \
+    -DWITH_DRWAVAUDIOIMPORTER=ON \
+    -DWITH_FAAD2AUDIOIMPORTER=OFF \
+    -DWITH_FREETYPEFONT=OFF \
+    -DWITH_GLSLANGSHADERCONVERTER=OFF \
+    -DWITH_HARFBUZZFONT=OFF \
+    -DWITH_ICOIMPORTER=ON \
+    -DWITH_JPEGIMAGECONVERTER=OFF \
+    -DWITH_JPEGIMPORTER=OFF \
+    -DWITH_KTXIMAGECONVERTER=ON \
+    -DWITH_KTXIMPORTER=ON \
+    -DWITH_MESHOPTIMIZERSCENECONVERTER=OFF \
+    -DWITH_MINIEXRIMAGECONVERTER=ON \
+    -DWITH_OPENEXRIMAGECONVERTER=OFF \
+    -DWITH_OPENEXRIMPORTER=OFF \
+    -DWITH_OPENGEXIMPORTER=ON \
+    -DWITH_PNGIMAGECONVERTER=OFF \
+    -DWITH_PNGIMPORTER=OFF \
+    -DWITH_PRIMITIVEIMPORTER=ON \
+    -DWITH_SPIRVTOOLSSHADERCONVERTER=OFF \
+    -DWITH_STANFORDIMPORTER=ON \
+    -DWITH_STANFORDSCENECONVERTER=ON \
+    -DWITH_STBDXTIMAGECONVERTER=ON \
+    -DWITH_STBIMAGECONVERTER=ON \
+    -DWITH_STBIMAGEIMPORTER=ON \
+    -DWITH_STBTRUETYPEFONT=ON \
+    -DWITH_STBVORBISAUDIOIMPORTER=ON \
+    -DWITH_STLIMPORTER=ON \
+    -DWITH_TINYGLTFIMPORTER=ON \
+    -DBUILD_STATIC=ON \
+    -DBUILD_TESTS=ON \
+    -DBUILD_GL_TESTS=ON \
+    -G Xcode
+set -o pipefail && cmake --build . --config Release | xcbeautify
+CORRADE_TEST_COLOR=ON ctest -V -C Release
+
+# Test install, after running the tests as for them it shouldn't be needed
+set -o pipefail && cmake --build . --config Release --target install | xcbeautify
