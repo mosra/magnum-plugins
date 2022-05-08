d4ee6e94665b6b78fb014046230682ebe6ba371f
Vladimír Vondruš <mosra@centrum.cz>
2022-05-08 10:47:08 +0200

    CgltfImporter: adapt test file conversion script to renames.
    Forgotten in 231458d997dedf199b70dab782cf6ebe5fa07bc5. Plus one file
    earned a newline on EOF during the rename, apparently, so reverting
    that to be consistent with what the script does.

--- src/MagnumPlugins/CgltfImporter/Test/convert.sh
+++ src/MagnumPlugins/CgltfImporter/Test/convert.sh
@@ -8,17 +8,17 @@ for i in *.bin.in; do
 done
 
 # gltf -> embedded gltf
-for i in animation buffer-short-size image image-buffer image-basis mesh skin; do
+for i in animation buffer-invalid-short-size image image-buffer image-basis mesh skin; do
     ./gltf2embedded.py ${i}.gltf
 done
 
 # gltf -> glb
-for i in animation animation-embedded buffer-short-size buffer-short-size-embedded buffer-wrong-size camera empty image image-embedded image-buffer image-buffer-embedded image-basis image-basis-embedded light mesh mesh-embedded scene scene-transformation skin skin-embedded texture-default-sampler texture-empty-sampler texture; do
+for i in animation animation-embedded buffer-invalid-short-size buffer-invalid-short-size-embedded buffer-long-size camera empty image image-embedded image-buffer image-buffer-embedded image-basis image-basis-embedded light mesh mesh-embedded scene scene-transformation skin skin-embedded texture-default-sampler texture-empty-sampler texture; do
     ./gltf2glb.py ${i}.gltf
 done
 
 # special cases
-./gltf2glb.py buffer-notfound.gltf --no-embed
-./gltf2glb.py buffer-no-uri.gltf --no-embed
+./gltf2glb.py buffer-invalid-notfound.gltf --no-embed
+./gltf2glb.py buffer-invalid-no-uri.gltf --no-embed
 ./gltf2glb.py external-data.gltf --no-embed
 ./gltf2glb.py external-data-order.gltf --no-embed
