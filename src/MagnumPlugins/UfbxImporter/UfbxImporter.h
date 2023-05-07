99f95282084122dd72b75c64b3732f22a2b3a888
bqqbarbhg <bqqbarbhg@gmail.com>
2023-05-07 20:11:25 +0300

    UfbxImporter: Documentation improvements

--- src/MagnumPlugins/UfbxImporter/UfbxImporter.h
+++ src/MagnumPlugins/UfbxImporter/UfbxImporter.h
@@ -250,7 +250,7 @@ setting any to zero disables loading any tangents etc.
 -   FBX represents rotations using Euler angles, which are converted to
     quaternions on import.
 -   Cubic FBX animation curves are resampled as linear interpolation as time
-    tangents prevent representing them as cubic hermite splines, the resampling
+    tangents prevent representing them as cubic Hermite splines, the resampling
     rate can be configured via the
     @cb{.ini} resampleRate @ce @ref Trade-UfbxImporter-configuration "configuration option"
 -   FBX represents rotations as Euler angles so even linear rotations are
@@ -267,12 +267,14 @@ setting any to zero disables loading any tangents etc.
     to import each animation layer as an individual animation. Currently this
     loses information about what layers animation stacks contain.
 -   As many FBX exporters resample animations, to avoid double resampling
-    @ref UfbxImporter will avoid closely spaced keyframes, this interval can be
-    adjusted via the
+    @ref UfbxImporter will avoid resampling closely spaced keyframes, this
+    interval can be adjusted via the
     @cb{.ini} minimumSampleRate @ce @ref Trade-UfbxImporter-configuration "configuration option".
 -   Constant keyframes are represented as nearly equal time keyframes, the
     constant interpolation interval can be adjusted via the
     @cb{.ini} constantInterpolationDuration @ce @ref Trade-UfbxImporter-configuration "configuration option".
+-   Skin deformers are supported but only the first skin deformer for a mesh is
+    imported currently.
 
 @subsection Trade-UfbxImporter-behavior-textures Texture import
 
