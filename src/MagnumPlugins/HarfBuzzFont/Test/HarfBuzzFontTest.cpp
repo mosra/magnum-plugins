mosra/magnum-plugins

commit a1976f586293d7ecd14107e4a34b3bfaf00d251d

*Font: adapt to Magnum::Text deSTLification.
Still some unnecessary std::vector usage due to GlyphCache and
TextureTools::atlas() not being cleaned up yet, and questionable
<algorithm> usage, but at least it's not full of std::string allocations
anymore.

src/MagnumPlugins/FreeTypeFont/FreeTypeFont.cpp
@@ -28,6 +28,8 @@
 #include <algorithm> /* std::transform(), std::sort(), std::unique() */
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include <Corrade/Containers/StringView.h>
+#include <Corrade/Containers/Triple.h>
 #include <Corrade/PluginManager/AbstractManager.h>
 #include <Corrade/Utility/Algorithms.h>
 #include <Corrade/Utility/Unicode.h>
@@ -42,15 +44,15 @@ namespace {
 
 class FreeTypeLayouter: public AbstractLayouter {
     public:
-        explicit FreeTypeLayouter(FT_Face font, const AbstractGlyphCache& cache, const Float fontSize, const Float textSize, std::vector<FT_UInt>&& glyphs);
+        explicit FreeTypeLayouter(FT_Face font, const AbstractGlyphCache& cache, const Float fontSize, const Float textSize, Containers::Array<FT_UInt>&& glyphs);
 
     private:
-        std::tuple<Range2D, Range2D, Vector2> doRenderGlyph(const UnsignedInt i) override;
+        Containers::Triple<Range2D, Range2D, Vector2> doRenderGlyph(const UnsignedInt i) override;
 
         FT_Face _font;
         const AbstractGlyphCache& _cache;
         const Float _fontSize, _layoutSize;
-        const std::vector<FT_UInt> _glyphs;
+        const Containers::Array<FT_UInt> _glyphs;
 };
 
 }
@@ -114,26 +116,25 @@ Vector2 FreeTypeFont::doGlyphAdvance(const UnsignedInt glyph) {
     return Vector2(ftFont->glyph->advance.x, ftFont->glyph->advance.y)/64.0f;
 }
 
-void FreeTypeFont::doFillGlyphCache(AbstractGlyphCache& cache, const std::u32string& characters) {
+void FreeTypeFont::doFillGlyphCache(AbstractGlyphCache& cache, const Containers::ArrayView<const char32_t> characters) {
     /** @bug Crash when atlas is too small */
 
     /* Get glyph codes from characters */
-    std::vector<FT_UInt> glyphIndices;
-    glyphIndices.resize(characters.size()+1);
+    Containers::Array<FT_UInt> glyphIndices{NoInit, characters.size() + 1};
     glyphIndices[0] = 0;
-    std::transform(characters.begin(), characters.end(), glyphIndices.begin()+1,
-        [this](const char32_t c) { return FT_Get_Char_Index(ftFont, c); });
+    for(std::size_t i = 0; i != characters.size(); ++i)
+        glyphIndices[i + 1] = FT_Get_Char_Index(ftFont, characters[i]);
 
     /* Remove duplicates (e.g. uppercase and lowercase mapped to same glyph) */
+    /** @todo deduplicate via a BitArray instead */
     std::sort(glyphIndices.begin(), glyphIndices.end());
-    glyphIndices.erase(std::unique(glyphIndices.begin(), glyphIndices.end()), glyphIndices.end());
+    const std::size_t uniqueCount = std::unique(glyphIndices.begin(), glyphIndices.end()) - glyphIndices.begin();
 
-    /* Sizes of all characters */
-    std::vector<Vector2i> glyphSizes;
-    glyphSizes.reserve(glyphIndices.size());
-    for(FT_UInt c: glyphIndices) {
-        CORRADE_INTERNAL_ASSERT_OUTPUT(FT_Load_Glyph(ftFont, c, FT_LOAD_DEFAULT) == 0);
-            glyphSizes.push_back(Vector2i(ftFont->glyph->metrics.width, ftFont->glyph->metrics.height)/64);
+    /* Sizes of all glyphs */
+    std::vector<Vector2i> glyphSizes(uniqueCount);
+    for(std::size_t i = 0; i != uniqueCount; ++i) {
+        CORRADE_INTERNAL_ASSERT_OUTPUT(FT_Load_Glyph(ftFont, glyphIndices[i], FT_LOAD_DEFAULT) == 0);
+            glyphSizes[i] = Vector2i{Int(ftFont->glyph->metrics.width), Int(ftFont->glyph->metrics.height)}/64;
     }
 
     /* Create texture atlas */
@@ -167,24 +168,23 @@ void FreeTypeFont::doFillGlyphCache(AbstractGlyphCache& cache, const std::u32str
     cache.setImage({}, image);
 }
 
-Containers::Pointer<AbstractLayouter> FreeTypeFont::doLayout(const AbstractGlyphCache& cache, const Float size, const std::string& text) {
+Containers::Pointer<AbstractLayouter> FreeTypeFont::doLayout(const AbstractGlyphCache& cache, const Float size, const Containers::StringView text) {
     /* Get glyph codes from characters */
-    std::vector<UnsignedInt> glyphs;
-    glyphs.reserve(text.size());
+    Containers::Array<UnsignedInt> glyphs{NoInit, text.size()};
     for(std::size_t i = 0; i != text.size(); ) {
-        UnsignedInt codepoint;
-        std::tie(codepoint, i) = Utility::Unicode::nextChar(text, i);
-        glyphs.push_back(FT_Get_Char_Index(ftFont, codepoint));
+        const Containers::Pair<char32_t, std::size_t> codepointNext = Utility::Unicode::nextChar(text, i);
+        glyphs[i] = FT_Get_Char_Index(ftFont, codepointNext.first());
+        i = codepointNext.second();
     }
 
-    return Containers::pointer(new FreeTypeLayouter(ftFont, cache, this->size(), size, Utility::move(glyphs)));
+    return Containers::pointer<FreeTypeLayouter>(ftFont, cache, this->size(), size, Utility::move(glyphs));
 }
 
 namespace {
 
-FreeTypeLayouter::FreeTypeLayouter(FT_Face font, const AbstractGlyphCache& cache, const Float fontSize, const Float layoutSize, std::vector<FT_UInt>&& glyphs): AbstractLayouter(glyphs.size()), _font(font), _cache(cache), _fontSize(fontSize), _layoutSize(layoutSize), _glyphs(Utility::move(glyphs)) {}
+FreeTypeLayouter::FreeTypeLayouter(FT_Face font, const AbstractGlyphCache& cache, const Float fontSize, const Float layoutSize, Containers::Array<FT_UInt>&& glyphs): AbstractLayouter{UnsignedInt(glyphs.size())}, _font{font}, _cache(cache), _fontSize{fontSize}, _layoutSize{layoutSize}, _glyphs{Utility::move(glyphs)} {}
 
-std::tuple<Range2D, Range2D, Vector2> FreeTypeLayouter::doRenderGlyph(const UnsignedInt i) {
+Containers::Triple<Range2D, Range2D, Vector2> FreeTypeLayouter::doRenderGlyph(const UnsignedInt i) {
     /* Position of the texture in the resulting glyph, texture coordinates */
     Vector2i position;
     Range2Di rectangle;
@@ -204,7 +204,7 @@ std::tuple<Range2D, Range2D, Vector2> FreeTypeLayouter::doRenderGlyph(const Unsi
     /* Glyph advance, denormalized to requested text size */
     const Vector2 advance = Vector2{Float(slot->advance.x), Float(slot->advance.y)}*(_layoutSize/(64.0f*_fontSize));
 
-    return std::make_tuple(quadRectangle, textureCoordinates, advance);
+    return {quadRectangle, textureCoordinates, advance};
 }
 
 }

src/MagnumPlugins/FreeTypeFont/Test/FreeTypeFontTest.cpp
@@ -153,30 +153,28 @@ void FreeTypeFontTest::layout() {
     CORRADE_COMPARE(layouter->glyphCount(), 4);
 
     Vector2 cursorPosition;
-    Range2D rectangle, position, textureCoordinates;
+    Range2D rectangle;
 
     /* 'W' */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(0, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D({0.78125f, 1.0625f}, {1.28125f, 4.8125f}));
-    CORRADE_COMPARE(textureCoordinates, Range2D({0, 0.03125f}, {0.0625f, 0.5f}));
+    CORRADE_COMPARE(layouter->renderGlyph(0, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{{0.78125f, 1.0625f}, {1.28125f, 4.8125f}},
+                         Range2D{{0, 0.03125f}, {0.0625f, 0.5f}}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.53125f, 0.0f));
 
     /* 'a' (not in cache) */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(1, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D());
-    CORRADE_COMPARE(textureCoordinates, Range2D());
+    CORRADE_COMPARE(layouter->renderGlyph(1, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.25f, 0.0f));
 
     /* 'v' (not in cache) */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(2, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D());
-    CORRADE_COMPARE(textureCoordinates, Range2D());
+    CORRADE_COMPARE(layouter->renderGlyph(2, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.25f, 0.0f));
 
     /* 'e' */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(3, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D({0.78125f, 0.375f}, {2.28125f, 1.25f}));
-    CORRADE_COMPARE(textureCoordinates, Range2D({0.0625f, 0.015625f}, {0.25f, 0.125f}));
+    CORRADE_COMPARE(layouter->renderGlyph(3, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{{0.78125f, 0.375f}, {2.28125f, 1.25f}},
+                         Range2D{{0.0625f, 0.015625f}, {0.25f, 0.125f}}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.28125f, 0.0f));
 }
 
@@ -231,7 +229,7 @@ void FreeTypeFontTest::fillGlyphCache() {
     CORRADE_COMPARE(cache[font->glyphId('g')], std::make_pair(
         Vector2i{0, -4}, Range2Di{{14, 16}, {23, 29}}));
     /* UTF-8 */
-    CORRADE_COMPARE(cache[font->glyphId(Utility::Unicode::nextChar("š", 0).first)], std::make_pair(
+    CORRADE_COMPARE(cache[font->glyphId(Utility::Unicode::nextChar("š", 0).first())], std::make_pair(
         Vector2i{}, Range2Di{{70, 64}, {78, 78}}));
 }
 

src/MagnumPlugins/HarfBuzzFont/Test/HarfBuzzFontTest.cpp
@@ -74,30 +74,27 @@ void HarfBuzzFontTest::layout() {
     CORRADE_COMPARE(layouter->glyphCount(), 4);
 
     Vector2 cursorPosition;
-    Range2D rectangle, position, textureCoordinates;
+    Range2D rectangle;
 
     /* Difference between this and FreeTypeFont should be _only_ in advances */
 
     /* 'W' */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(0, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D({0.78125f, 1.0625f}, {1.28125f, 4.8125f}));
-    CORRADE_COMPARE(textureCoordinates, Range2D({0, 0.03125f}, {0.0625f, 0.5f}));
+    CORRADE_COMPARE(layouter->renderGlyph(0, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{{0.78125f, 1.0625f}, {1.28125f, 4.8125f}},
+                         Range2D{{0, 0.03125f}, {0.0625f, 0.5f}}));
     /* Versions 3.3.0 and 3.3.1 reported {0.515625f, 0.0f} here, but the change
        is reverted in 3.3.2 again "as it proved problematic". */
     CORRADE_COMPARE(cursorPosition, Vector2(0.51123f, 0.0f));
 
     /* 'a' (not in cache) */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(1, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D());
-    CORRADE_COMPARE(textureCoordinates, Range2D());
+    CORRADE_COMPARE(layouter->renderGlyph(1, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.258301f, 0.0f));
 
-    /* 'v' (not in cache) */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(2, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D());
-    CORRADE_COMPARE(textureCoordinates, Range2D());
-
-    /* HarfBuzz before 1.7 and after 3.1 give 0.25, versions between the other */
+    /* 'v' (not in cache). HarfBuzz before 1.7 and after 3.1 give 0.25,
+       versions between the other. */
+    CORRADE_COMPARE(layouter->renderGlyph(2, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{}, Range2D{}));
     if((HB_VERSION_MAJOR*100 + HB_VERSION_MINOR < 107) ||
        (HB_VERSION_MAJOR*100 + HB_VERSION_MINOR >= 301))
         CORRADE_COMPARE(cursorPosition, Vector2(0.25f, 0.0f));
@@ -105,9 +102,9 @@ void HarfBuzzFontTest::layout() {
         CORRADE_COMPARE(cursorPosition, Vector2(0.249512f, 0.0f));
 
     /* 'e' */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(3, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D({0.78125f, 0.375f}, {2.28125f, 1.25f}));
-    CORRADE_COMPARE(textureCoordinates, Range2D({0.0625f, 0.015625f}, {0.25f, 0.125f}));
+    CORRADE_COMPARE(layouter->renderGlyph(3, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{{0.78125f, 0.375f}, {2.28125f, 1.25f}},
+                         Range2D{{0.0625f, 0.015625f}, {0.25f, 0.125f}}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.260742f, 0.0f));
 }
 

src/MagnumPlugins/StbTrueTypeFont/Test/StbTrueTypeFontTest.cpp
@@ -161,7 +161,7 @@ void StbTrueTypeFontTest::layout() {
     CORRADE_COMPARE(layouter->glyphCount(), 4);
 
     Vector2 cursorPosition;
-    Range2D rectangle, position, textureCoordinates;
+    Range2D rectangle;
 
     /* Compared to FreeType, StbTrueType has slightly larger glyphs which makes
        the test values quite different but the actual visual output isn't that
@@ -170,27 +170,25 @@ void StbTrueTypeFontTest::layout() {
        characters look mostly the same but occupy more space. */
 
     /* 'W' */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(0, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D({0.78125f, 1.0625f}, {1.28125f, 4.8125f}));
-    CORRADE_COMPARE(textureCoordinates, Range2D({0, 0.03125f}, {0.0625f, 0.5f}));
+    CORRADE_COMPARE(layouter->renderGlyph(0, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{{0.78125f, 1.0625f}, {1.28125f, 4.8125f}},
+                         Range2D{{0, 0.03125f}, {0.0625f, 0.5f}}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.595917f, 0.0f));
 
     /* 'a' (not in cache) */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(1, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D());
-    CORRADE_COMPARE(textureCoordinates, Range2D());
+    CORRADE_COMPARE(layouter->renderGlyph(1, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.295582f, 0.0f));
 
     /* 'v' (not in cache) */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(2, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D());
-    CORRADE_COMPARE(textureCoordinates, Range2D());
+    CORRADE_COMPARE(layouter->renderGlyph(2, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.289709f, 0.0f));
 
     /* 'e' */
-    std::tie(position, textureCoordinates) = layouter->renderGlyph(3, cursorPosition = {}, rectangle);
-    CORRADE_COMPARE(position, Range2D({0.78125f, 0.375f}, {2.28125f, 1.25f}));
-    CORRADE_COMPARE(textureCoordinates, Range2D({0.0625f, 0.015625f}, {0.25f, 0.125f}));
+    CORRADE_COMPARE(layouter->renderGlyph(3, cursorPosition = {}, rectangle),
+        Containers::pair(Range2D{{0.78125f, 0.375f}, {2.28125f, 1.25f}},
+                         Range2D{{0.0625f, 0.015625f}, {0.25f, 0.125f}}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.298658f, 0.0f));
 }
 
@@ -247,7 +245,7 @@ void StbTrueTypeFontTest::fillGlyphCache() {
     CORRADE_COMPARE(cache[font->glyphId('g')], std::make_pair(
         Vector2i{0, -5}, Range2Di{{15, 19}, {25, 35}}));
     /* UTF-8 */
-    CORRADE_COMPARE(cache[font->glyphId(Utility::Unicode::nextChar("š", 0).first)], std::make_pair(
+    CORRADE_COMPARE(cache[font->glyphId(Utility::Unicode::nextChar("š", 0).first())], std::make_pair(
         Vector2i{0, -1}, Range2Di{{75, 76}, {83, 92}}));
 }
 

src/MagnumPlugins/FreeTypeFont/FreeTypeFont.h
@@ -166,9 +166,9 @@ class MAGNUM_FREETYPEFONT_EXPORT FreeTypeFont: public AbstractFont {
         Vector2 doGlyphAdvance(UnsignedInt glyph) override;
 
         /** @todo Why this can't be defined as local? */
-        void doFillGlyphCache(AbstractGlyphCache& cache, const std::u32string& characters) override;
+        void doFillGlyphCache(AbstractGlyphCache& cache, Containers::ArrayView<const char32_t> characters) override;
 
-        Containers::Pointer<AbstractLayouter> MAGNUM_FREETYPEFONT_LOCAL doLayout(const AbstractGlyphCache& cache, Float size, const std::string& text) override;
+        Containers::Pointer<AbstractLayouter> MAGNUM_FREETYPEFONT_LOCAL doLayout(const AbstractGlyphCache& cache, Float size, Containers::StringView text) override;
 };
 
 }}

src/MagnumPlugins/HarfBuzzFont/HarfBuzzFont.cpp
@@ -26,6 +26,8 @@
 #include "HarfBuzzFont.h"
 
 #include <hb-ft.h>
+#include <Corrade/Containers/StringView.h>
+#include <Corrade/Containers/Triple.h>
 #include <Corrade/PluginManager/AbstractManager.h>
 #include <Magnum/Text/AbstractGlyphCache.h>
 
@@ -40,7 +42,7 @@ class HarfBuzzLayouter: public AbstractLayouter {
         ~HarfBuzzLayouter();
 
     private:
-        std::tuple<Range2D, Range2D, Vector2> doRenderGlyph(UnsignedInt i) override;
+        Containers::Triple<Range2D, Range2D, Vector2> doRenderGlyph(UnsignedInt i) override;
 
         const AbstractGlyphCache& cache;
         const Float fontSize, textSize;
@@ -80,7 +82,7 @@ void HarfBuzzFont::doClose() {
     FreeTypeFont::doClose();
 }
 
-Containers::Pointer<AbstractLayouter> HarfBuzzFont::doLayout(const AbstractGlyphCache& cache, const Float size, const std::string& text) {
+Containers::Pointer<AbstractLayouter> HarfBuzzFont::doLayout(const AbstractGlyphCache& cache, const Float size, const Containers::StringView text) {
     /* Prepare HarfBuzz buffer */
     hb_buffer_t* const buffer = hb_buffer_create();
     hb_buffer_set_direction(buffer, HB_DIRECTION_LTR);
@@ -107,7 +109,7 @@ HarfBuzzLayouter::~HarfBuzzLayouter() {
     hb_buffer_destroy(buffer);
 }
 
-std::tuple<Range2D, Range2D, Vector2> HarfBuzzLayouter::doRenderGlyph(const UnsignedInt i) {
+Containers::Triple<Range2D, Range2D, Vector2> HarfBuzzLayouter::doRenderGlyph(const UnsignedInt i) {
     /* Position of the texture in the resulting glyph, texture coordinates */
     Vector2i position;
     Range2Di rectangle;
@@ -129,7 +131,7 @@ std::tuple<Range2D, Range2D, Vector2> HarfBuzzLayouter::doRenderGlyph(const Unsi
     const Vector2 advance = Vector2(glyphPositions[i].x_advance,
                                     glyphPositions[i].y_advance)*(textSize/(64.0f*fontSize));
 
-    return std::make_tuple(quadRectangle, textureCoordinates, advance);
+    return {quadRectangle, textureCoordinates, advance};
 }
 
 }

src/MagnumPlugins/HarfBuzzFont/HarfBuzzFont.h
@@ -150,7 +150,7 @@ class MAGNUM_HARFBUZZFONT_EXPORT HarfBuzzFont: public FreeTypeFont {
         MAGNUM_HARFBUZZFONT_LOCAL bool doIsOpened() const override;
         MAGNUM_HARFBUZZFONT_LOCAL Metrics doOpenData(Containers::ArrayView<const char> data, Float size) override;
         MAGNUM_HARFBUZZFONT_LOCAL void doClose() override;
-        MAGNUM_HARFBUZZFONT_LOCAL Containers::Pointer<AbstractLayouter> doLayout(const AbstractGlyphCache& cache, Float size, const std::string& text) override;
+        MAGNUM_HARFBUZZFONT_LOCAL Containers::Pointer<AbstractLayouter> doLayout(const AbstractGlyphCache& cache, Float size, Containers::StringView text) override;
 
         hb_font_t* hbFont;
 };

commit 95a2cfec4f1b46b0f34bf3ab3d3d8c7a176d120d

*Font: explicitly test UTF-8 layouting.
Yet another case of "100%" coverage, of course I broke this in
FreeTypeFont in a1976f586293d7ecd14107e4a34b3bfaf00d251d when removing
STL usage. Sigh.

src/MagnumPlugins/FreeTypeFont/FreeTypeFont.cpp
@@ -29,6 +29,7 @@
 #include <tuple> /* std::tie() :( */
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include <Corrade/Containers/GrowableArray.h>
 #include <Corrade/Containers/StringView.h>
 #include <Corrade/Containers/Triple.h>
 #include <Corrade/PluginManager/AbstractManager.h>
@@ -183,10 +184,11 @@ void FreeTypeFont::doFillGlyphCache(AbstractGlyphCache& cache, const Containers:
 
 Containers::Pointer<AbstractLayouter> FreeTypeFont::doLayout(const AbstractGlyphCache& cache, const Float size, const Containers::StringView text) {
     /* Get glyph codes from characters */
-    Containers::Array<UnsignedInt> glyphs{NoInit, text.size()};
+    Containers::Array<UnsignedInt> glyphs;
+    arrayReserve(glyphs, text.size());
     for(std::size_t i = 0; i != text.size(); ) {
         const Containers::Pair<char32_t, std::size_t> codepointNext = Utility::Unicode::nextChar(text, i);
-        glyphs[i] = FT_Get_Char_Index(ftFont, codepointNext.first());
+        arrayAppend(glyphs, FT_Get_Char_Index(ftFont, codepointNext.first()));
         i = codepointNext.second();
     }
 

src/MagnumPlugins/FreeTypeFont/Test/FreeTypeFontTest.cpp
@@ -59,6 +59,14 @@ struct FreeTypeFontTest: TestSuite::Tester {
 
 const struct {
     const char* name;
+    const char* string;
+} LayoutData[]{
+    {"", "Wave"},
+    {"UTF-8", "Wavě"},
+};
+
+const struct {
+    const char* name;
     const char* characters;
 } FillGlyphCacheData[]{
     {"",
@@ -78,8 +86,10 @@ FreeTypeFontTest::FreeTypeFontTest() {
     addTests({&FreeTypeFontTest::empty,
               &FreeTypeFontTest::invalid,
 
-              &FreeTypeFontTest::properties,
-              &FreeTypeFontTest::layout});
+              &FreeTypeFontTest::properties});
+
+    addInstancedTests({&FreeTypeFontTest::layout},
+        Containers::arraySize(LayoutData));
 
     addInstancedTests({&FreeTypeFontTest::fillGlyphCache},
         Containers::arraySize(FillGlyphCacheData));
@@ -138,6 +148,9 @@ void FreeTypeFontTest::properties() {
 }
 
 void FreeTypeFontTest::layout() {
+    auto&& data = LayoutData[testCaseInstanceId()];
+    setTestCaseDescription(data.name);
+
     Containers::Pointer<AbstractFont> font = _manager.instantiate("FreeTypeFont");
     CORRADE_VERIFY(font->openFile(Utility::Path::join(FREETYPEFONT_TEST_DIR, "Oxygen.ttf"), 16.0f));
 
@@ -150,8 +163,20 @@ void FreeTypeFontTest::layout() {
     } cache{Vector2i{256}};
     cache.insert(font->glyphId(U'W'), {25, 34}, {{0, 8}, {16, 128}});
     cache.insert(font->glyphId(U'e'), {25, 12}, {{16, 4}, {64, 32}});
+    /* ě has deliberately the same glyph data as e */
+    cache.insert(font->glyphId(
+        /* MSVC (but not clang-cl) doesn't support UTF-8 in char32_t literals
+           but it does it regular strings. Still a problem in MSVC 2022, what a
+           trash fire, can't you just give up on those codepage insanities
+           already, ffs?! */
+        #if defined(CORRADE_TARGET_MSVC) && !defined(CORRADE_TARGET_CLANG)
+        U'\u011B'
+        #else
+        U'ě'
+        #endif
+    ), {25, 12}, {{16, 4}, {64, 32}});
 
-    Containers::Pointer<AbstractLayouter> layouter = font->layout(cache, 0.5f, "Wave");
+    Containers::Pointer<AbstractLayouter> layouter = font->layout(cache, 0.5f, data.string);
     CORRADE_VERIFY(layouter);
     CORRADE_COMPARE(layouter->glyphCount(), 4);
 
@@ -174,7 +199,7 @@ void FreeTypeFontTest::layout() {
         Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.25f, 0.0f));
 
-    /* 'e' */
+    /* 'e' or 'ě' */
     CORRADE_COMPARE(layouter->renderGlyph(3, cursorPosition = {}, rectangle),
         Containers::pair(Range2D{{0.78125f, 0.375f}, {2.28125f, 1.25f}},
                          Range2D{{0.0625f, 0.015625f}, {0.25f, 0.125f}}));

src/MagnumPlugins/HarfBuzzFont/Test/HarfBuzzFontTest.cpp
@@ -45,8 +45,18 @@ struct HarfBuzzFontTest: TestSuite::Tester {
     PluginManager::Manager<AbstractFont> _manager{"nonexistent"};
 };
 
+const struct {
+    const char* name;
+    const char* string;
+    Float advanceAfterV, advanceAfterVWeird;
+} LayoutData[]{
+    {"", "Wave", 0.25f, 0.249512f},
+    {"UTF-8", "Wavě", 0.25293f, 0.25293f},
+};
+
 HarfBuzzFontTest::HarfBuzzFontTest() {
-    addTests({&HarfBuzzFontTest::layout});
+    addInstancedTests({&HarfBuzzFontTest::layout},
+        Containers::arraySize(LayoutData));
 
     /* Load the plugin directly from the build tree. Otherwise it's static and
        already loaded. */
@@ -57,6 +67,9 @@ HarfBuzzFontTest::HarfBuzzFontTest() {
 }
 
 void HarfBuzzFontTest::layout() {
+    auto&& data = LayoutData[testCaseInstanceId()];
+    setTestCaseDescription(data.name);
+
     Containers::Pointer<AbstractFont> font = _manager.instantiate("HarfBuzzFont");
     CORRADE_VERIFY(font->openFile(Utility::Path::join(FREETYPEFONT_TEST_DIR, "Oxygen.ttf"), 16.0f));
 
@@ -69,8 +82,20 @@ void HarfBuzzFontTest::layout() {
     } cache{Vector2i{256}};
     cache.insert(font->glyphId(U'W'), {25, 34}, {{0, 8}, {16, 128}});
     cache.insert(font->glyphId(U'e'), {25, 12}, {{16, 4}, {64, 32}});
+    /* ě has deliberately the same glyph data as e */
+    cache.insert(font->glyphId(
+        /* MSVC (but not clang-cl) doesn't support UTF-8 in char32_t literals
+           but it does it regular strings. Still a problem in MSVC 2022, what a
+           trash fire, can't you just give up on those codepage insanities
+           already, ffs?! */
+        #if defined(CORRADE_TARGET_MSVC) && !defined(CORRADE_TARGET_CLANG)
+        U'\u011B'
+        #else
+        U'ě'
+        #endif
+    ), {25, 12}, {{16, 4}, {64, 32}});
 
-    Containers::Pointer<AbstractLayouter> layouter = font->layout(cache, 0.5f, "Wave");
+    Containers::Pointer<AbstractLayouter> layouter = font->layout(cache, 0.5f, data.string);
     CORRADE_VERIFY(layouter);
     CORRADE_COMPARE(layouter->glyphCount(), 4);
 
@@ -93,16 +118,17 @@ void HarfBuzzFontTest::layout() {
     CORRADE_COMPARE(cursorPosition, Vector2(0.258301f, 0.0f));
 
     /* 'v' (not in cache). HarfBuzz before 1.7 and after 3.1 give 0.25,
-       versions between the other. */
+       versions between the other; additionally vě has slightly different
+       spacing than ve. */
     CORRADE_COMPARE(layouter->renderGlyph(2, cursorPosition = {}, rectangle),
         Containers::pair(Range2D{}, Range2D{}));
     if((HB_VERSION_MAJOR*100 + HB_VERSION_MINOR < 107) ||
        (HB_VERSION_MAJOR*100 + HB_VERSION_MINOR >= 301))
-        CORRADE_COMPARE(cursorPosition, Vector2(0.25f, 0.0f));
+        CORRADE_COMPARE(cursorPosition, Vector2(data.advanceAfterV, 0.0f));
     else
-        CORRADE_COMPARE(cursorPosition, Vector2(0.249512f, 0.0f));
+        CORRADE_COMPARE(cursorPosition, Vector2(data.advanceAfterVWeird, 0.0f));
 
-    /* 'e' */
+    /* 'e' or 'ě' */
     CORRADE_COMPARE(layouter->renderGlyph(3, cursorPosition = {}, rectangle),
         Containers::pair(Range2D{{0.78125f, 0.375f}, {2.28125f, 1.25f}},
                          Range2D{{0.0625f, 0.015625f}, {0.25f, 0.125f}}));

src/MagnumPlugins/StbTrueTypeFont/Test/StbTrueTypeFontTest.cpp
@@ -59,6 +59,14 @@ struct StbTrueTypeFontTest: TestSuite::Tester {
 
 const struct {
     const char* name;
+    const char* string;
+} LayoutData[]{
+    {"", "Wave"},
+    {"UTF-8", "Wavě"},
+};
+
+const struct {
+    const char* name;
     const char* characters;
 } FillGlyphCacheData[]{
     {"",
@@ -78,8 +86,10 @@ StbTrueTypeFontTest::StbTrueTypeFontTest() {
     addTests({&StbTrueTypeFontTest::empty,
               &StbTrueTypeFontTest::invalid,
 
-              &StbTrueTypeFontTest::properties,
-              &StbTrueTypeFontTest::layout});
+              &StbTrueTypeFontTest::properties});
+
+    addInstancedTests({&StbTrueTypeFontTest::layout},
+        Containers::arraySize(LayoutData));
 
     addInstancedTests({&StbTrueTypeFontTest::fillGlyphCache},
         Containers::arraySize(FillGlyphCacheData));
@@ -146,6 +156,9 @@ void StbTrueTypeFontTest::properties() {
 }
 
 void StbTrueTypeFontTest::layout() {
+    auto&& data = LayoutData[testCaseInstanceId()];
+    setTestCaseDescription(data.name);
+
     Containers::Pointer<AbstractFont> font = _manager.instantiate("StbTrueTypeFont");
     CORRADE_VERIFY(font->openFile(Utility::Path::join(FREETYPEFONT_TEST_DIR, "Oxygen.ttf"), 16.0f));
 
@@ -158,8 +171,20 @@ void StbTrueTypeFontTest::layout() {
     } cache{Vector2i{256}};
     cache.insert(font->glyphId(U'W'), {25, 34}, {{0, 8}, {16, 128}});
     cache.insert(font->glyphId(U'e'), {25, 12}, {{16, 4}, {64, 32}});
+    /* ě has deliberately the same glyph data as e */
+    cache.insert(font->glyphId(
+        /* MSVC (but not clang-cl) doesn't support UTF-8 in char32_t literals
+           but it does it regular strings. Still a problem in MSVC 2022, what a
+           trash fire, can't you just give up on those codepage insanities
+           already, ffs?! */
+        #if defined(CORRADE_TARGET_MSVC) && !defined(CORRADE_TARGET_CLANG)
+        U'\u011B'
+        #else
+        U'ě'
+        #endif
+    ), {25, 12}, {{16, 4}, {64, 32}});
 
-    Containers::Pointer<AbstractLayouter> layouter = font->layout(cache, 0.5f, "Wave");
+    Containers::Pointer<AbstractLayouter> layouter = font->layout(cache, 0.5f, data.string);
     CORRADE_VERIFY(layouter);
     CORRADE_COMPARE(layouter->glyphCount(), 4);
 
@@ -188,7 +213,7 @@ void StbTrueTypeFontTest::layout() {
         Containers::pair(Range2D{}, Range2D{}));
     CORRADE_COMPARE(cursorPosition, Vector2(0.289709f, 0.0f));
 
-    /* 'e' */
+    /* 'e' or 'ě' */
     CORRADE_COMPARE(layouter->renderGlyph(3, cursorPosition = {}, rectangle),
         Containers::pair(Range2D{{0.78125f, 0.375f}, {2.28125f, 1.25f}},
                          Range2D{{0.0625f, 0.015625f}, {0.25f, 0.125f}}));