99236f432e78b8fe70c252c6c2d68220862a215b
Vladimír Vondruš <mosra@centrum.cz>
2024-10-11 15:15:45 +0200

    StbImageConverter: minor.

--- src/MagnumPlugins/StbImageConverter/StbImageConverter.cpp
+++ src/MagnumPlugins/StbImageConverter/StbImageConverter.cpp
@@ -220,7 +220,7 @@ bool StbImageConverter::doConvertToFile(const ImageView2D& image, const Containe
         else if(normalizedExtension == ".tga"_s ||
                 normalizedExtension == ".vda"_s ||
                 normalizedExtension == ".icb"_s ||
-                normalizedExtension ==  ".vst"_s)
+                normalizedExtension == ".vst"_s)
             _format = Format::Tga;
         else {
             Error{} << "Trade::StbImageConverter::convertToFile(): cannot determine output format for" << Utility::Path::split(filename).second() << "(plugin loaded as" << plugin() << Error::nospace << ", use one of the Stb{Bmp,Hdr,Jpeg,Png,Tga}ImageConverter aliases or a corresponding file extension)";
